2dba817487633a14f313ebb95b670b476b4c9577
Jules Clements <cdaf@outlook.co.nz>
2016-05-04 13:19:00 +1200

    Correct argument processing logic, i.e. if a completion command is passed, don't use default.

--- automation/provisioning/SysPrep.ps1
+++ automation/provisioning/SysPrep.ps1
@@ -6,7 +6,7 @@ Write-Host
 Write-Host "[$scriptName] ---------- start ----------"
 $unattendFile = $args[0]
 if ($unattendFile) {
-    Write-Host "[$scriptName] unattendFile : $unattendFile"
+    Write-Host "[$scriptName] unattendFile   : $unattendFile"
 } else {
     Write-Host "[$scriptName] unattendFile not supplied, halt!"
     exit 100
@@ -14,15 +14,17 @@ if ($unattendFile) {
 
 $completeAction = $args[1]
 if ($completeAction) {
+    Write-Host "[$scriptName] completeAction : $completeAction (choices shutdown, quit or reboot)"
+} else {
 	$completeAction = 'reboot'
     Write-Host "[$scriptName] completeAction : $completeAction (default, choices shutdown, quit or reboot)"
-} else {
-    Write-Host "[$scriptName] completeAction : $completeAction (choices shutdown, quit or reboot)"
-    exit 100
 }
 
+Write-Host "[$scriptName] Asynchronous Sysem Preparation ..."
 Write-Host "[$scriptName] C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /quiet /unattend:$unattendFile /$completeAction"
-$process = Start-Process -FilePath 'C:\Windows\System32\Sysprep\Sysprep.exe' -ArgumentList "/generalize /oobe /quiet /unattend:$unattendFile /$completeAction" -PassThru -Wait
+$process = Start-Process -FilePath 'C:\Windows\System32\Sysprep\Sysprep.exe' -ArgumentList "/generalize /oobe /quiet /unattend:$unattendFile /$completeAction" -PassThru
+
+sleep 15
 
 Write-Host "[$scriptName] ---------- stop -----------"
 Write-Host
