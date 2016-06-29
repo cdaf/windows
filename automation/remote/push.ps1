fbfdc4fffd6ece0259d00561f9f1cedb56b0a3f6
Jules Clements <cdaf@outlook.co.nz>
2016-06-30 09:18:47 +1200

    Standardised invoke for NuGet push process, i.e. Octopus

--- automation/remote/push.ps1
+++ automation/remote/push.ps1
@@ -1,9 +1,18 @@
+function executeExpression ($expression) {
+	Write-Host "[$scriptName] $expression"
+	try {
+		Invoke-Expression $expression
+	    if(!$?) { exit 1 }
+	} catch { exit 2 }
+    if ( $error[0] ) { exit 3 }
+}
+
 # The API Key needs to be obtained from the NuGet server to all package push without prompting for credentials
 $PACK_LOC     = $args[0]
 $API_KEY_FILE = $args[1]
 $NUGET_URL    = $args[2]
 
-$scriptName = $MyInvocation.MyCommand.Name
+$scriptName = 'push.ps1'
 
 Write-Host "[$scriptName] PACK_LOC     : $PACK_LOC"
 Write-Host "[$scriptName] API_KEY_FILE : $API_KEY_FILE"
@@ -13,5 +22,4 @@ $secure = Get-Content $API_KEY_FILE | ConvertTo-SecureString
 $plain = (New-Object System.Management.Automation.PSCredential 'N/A', $secure).GetNetworkCredential().Password
 
 # Assumes the package location is cleaned and there will be only one eligable 
-Write-Host "[$scriptName] NuGet.exe push $PACK_LOC\*.nupkg -ApiKey `$plain -Source $NUGET_URL"
-NuGet.exe push $PACK_LOC\*.nupkg -ApiKey $plain -Source $NUGET_URL
+executeExpression "NuGet.exe push $PACK_LOC\*.nupkg -ApiKey `$plain -Source $NUGET_URL"
