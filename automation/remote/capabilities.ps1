e169d7e59d8fcd46c1d489a93a7bf2d1f170b864
Jules Clements <cdaf@outlook.co.nz>
2016-11-21 17:50:59 +1300

    Improve Capabilities listings for .NET

--- automation/remote/capabilities.ps1
+++ automation/remote/capabilities.ps1
@@ -19,8 +19,24 @@ Write-Host "[$scriptName] List the PowerShell version"
 	write-host "  PSVersion.Major         : $($PSVersionTable.PSVersion.Major)"
 
 Write-Host
-Write-Host "[$scriptName] List the .NET Versions (4.5 or higher required)"
-Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse | Get-ItemProperty -name Version -EA 0 | Where { $_.PSChildName -match '^(?!S)\p{L}'} | Select PSChildName, Version
+Write-Host "[$scriptName] List the .NET Versions"
+Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
+Get-ItemProperty -name Version,Release -EA 0 |
+Where { $_.PSChildName -match '^(?!S)\p{L}'} |
+Select PSChildName, Version, Release, @{
+  name="Product"
+  expression={
+      switch -regex ($_.Release) {
+        "378389" { [Version]"4.5" }
+        "378675|378758" { [Version]"4.5.1" }
+        "379893" { [Version]"4.5.2" }
+        "393295|393297" { [Version]"4.6" }
+        "394254|394271" { [Version]"4.6.1" }
+        "394802|394806" { [Version]"4.6.2" }
+        {$_ -gt 394806} { [Version]"Undocumented 4.6.2 or higher, please update script" }
+      }
+    }
+}
 
 Write-Host
 Write-Host "[$scriptName] List the WIF Installed Versions"
@@ -37,7 +53,6 @@ Write-Host "[$scriptName] List installed MVC products (not applicable after MVC4
 if (Test-Path 'C:\Program Files (x86)\Microsoft ASP.NET' ) {
 	Get-ChildItem 'C:\Program Files (x86)\Microsoft ASP.NET'
 } else {
-	Write-Host
 	Write-Host "  MVC not explicitely installed (not required for MVC 5 and above)"
 }
 
@@ -47,7 +62,6 @@ $regkey = 'HKLM:\Software\Microsoft\IIS Extensions\MSDeploy'
 if ( Test-Path $regkey ) { 
 	Get-ChildItem $regkey
 } else {
-	Write-Host
 	Write-Host "  Web Deploy not installed ($regkey)"
 }
 
@@ -60,7 +74,6 @@ if ( $javaVersion ) {
 	Write-Host "[$scriptName] Java not installed"
 }
 
-Write-Host
 $javaVersion = cmd /c javac -version 2`>`&1
 if ( $javaVersion ) { 
 	Write-Host "[$scriptName] Java Compiler : $javaVersion"
@@ -68,7 +81,6 @@ if ( $javaVersion ) {
 	Write-Host "[$scriptName] Java Compiler not installed"
 }
 
-Write-Host
 $mavenVersion = cmd /c mvn --version 2`>`&1
 $mavenVersion = $mavenVersion | Select-String -Pattern 'ersion'
 if ( $mavenVersion ) { 
@@ -77,7 +89,6 @@ if ( $mavenVersion ) {
 	Write-Host "[$scriptName] Maven Compiler not installed"
 }
 
-Write-Host
 $nugetVersion = cmd /c NuGet 2`>`&1
 $nugetVersion = $nugetVersion | Select-String -Pattern 'ersion'
 if ( $nugetVersion ) { 
@@ -88,7 +99,6 @@ if ( $nugetVersion ) {
 	Write-Host "[$scriptName] NuGet not installed"
 }
 
-Write-Host
 $zipVersion = cmd /c 7za.exe i 2`>`&1
 $zipVersion = $zipVersion | Select-String -Pattern '7-Zip'
 if ( $zipVersion ) { 
@@ -97,7 +107,6 @@ if ( $zipVersion ) {
 	Write-Host "[$scriptName] 7Zip Command line not installed"
 }
 
-Write-Host
 $curlVersion = cmd /c curl.exe --version 2`>`&1
 $curlVersion = $curlVersion | Select-String -Pattern 'libcurl'
 if ( $curlVersion ) { 
@@ -106,7 +115,6 @@ if ( $curlVersion ) {
 	Write-Host "[$scriptName] curl.exe not installed"
 }
 
-Write-Host
 Write-Host "[$scriptName] List the build tools"
 $regkey = 'HKLM:\Software\Microsoft\MSBuild\ToolsVersions'
 if ( Test-Path $regkey ) { 
