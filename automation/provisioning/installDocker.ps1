2c7ae1b1f95c73ba75ca2690d968dafab14567dd
Jules Clements <cdaf@outlook.co.nz>
2017-02-15 20:54:53 +1300

    1.4.6.15 Docker installer

---
+++ automation/provisioning/installDocker.ps1
@@ -0,0 +1,31 @@
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$wait = 10
+	$retryMax = 30
+	$retryCount = 0
+	while (( $retryCount -lt $retryMax ) -and ($exitCode -ne 0)) {
+		$exitCode = 0
+		$error.clear()
+		Write-Host "[$scriptName][$retryCount] $expression"
+		try {
+			Invoke-Expression $expression
+		    if(!$?) { Write-Host "[$scriptName] `$? = $?"; $exitCode = 1 }
+		} catch { echo $_.Exception|format-list -force; $exitCode = 2 }
+	    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; $exitCode = 3 }
+	    if ($exitCode -gt 0) {
+			$retryCount += 1
+			Write-Host "[$scriptName] Wait $wait seconds, then retry $retryCount of $retryMax"
+			sleep $wait
+		}
+    }
+}
+
+$scriptName = 'installDocker.ps1'
+Write-Host
+Write-Host "[$scriptName] ---------- start ----------"
+
+executeExpression  "Install-Module -Name DockerMsftProvider -Repository PSGallery -Force"
+executeExpression  "Install-Package -Name docker -ProviderName DockerMsftProvider -Force"
+
+Write-Host
+Write-Host "[$scriptName] ---------- stop ----------"
