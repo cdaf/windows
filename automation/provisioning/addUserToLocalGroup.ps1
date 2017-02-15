426dc85d8b00bab1ce3f0c3802b5b4caf86f345c
Jules Clements <cdaf@outlook.co.nz>
2017-02-15 18:27:39 +1300

    1.4.6.14 add support for adding a local user to a group. Rollup version for removal of 7zip dependancy in storeArtefacts.

--- automation/provisioning/addUserToLocalGroup.ps1
+++ automation/provisioning/addUserToLocalGroup.ps1
@@ -1,6 +1,15 @@
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$error.clear()
+	Write-Host "[$scriptName] $expression"
+	try {
+		Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
+	} catch { echo $_.Exception|format-list -force; exit 2 }
+    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+}
+
 $scriptName = 'addUserToLocalGroup.ps1'
-Write-Host
-Write-Host "[$scriptName] Add a user to the local group"
 Write-Host
 Write-Host "[$scriptName] ---------- start ----------"
 $group = $args[0]
@@ -23,14 +32,17 @@ $domain = $args[2]
 if ($domain) {
     Write-Host "[$scriptName] domain   : $domain"
 } else {
-	$domain = 'SKY'
-    Write-Host "[$scriptName] domain   : $domain (default)"
+    Write-Host "[$scriptName] domain   : not supplied, will treat as local machine (workgroup)"
 }
 
-Write-Host
-Write-Host "[$scriptName] Add $domain/$userName to local group $group."
-$de = [ADSI]"WinNT://$env:computername/$group,group"
-$de.psbase.Invoke("Add",([ADSI]"WinNT://$domain/$userName").path)
+if ($domain) {
+	Write-Host
+	Write-Host "[$scriptName] Add $domain/$userName to local group $group."
+	$de = [ADSI]"WinNT://$env:computername/$group,group"
+	$de.psbase.Invoke("Add",([ADSI]"WinNT://$domain/$userName").path)
+} else {
+	executeExpression "net localgroup `"Remote $group Users`" $userName /add"
+}
 
 Write-Host
 Write-Host "[$scriptName] ---------- stop ----------"
