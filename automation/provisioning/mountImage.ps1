3490630ae2b62d88be43f376001f0da762c0d5e2
Jules Clements <cdaf@outlook.co.nz>
2017-02-28 13:13:54 +1300

    1.5.0.04 extended to support website or file system as source

--- automation/provisioning/mountImage.ps1
+++ automation/provisioning/mountImage.ps1
@@ -9,6 +9,8 @@ function executeExpression ($expression) {
     if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
 }
 
+# This script is designed for media that is on a file share or web server, it will download the media to the
+# local file system tehn mount it.
 $scriptName = 'mountImage.ps1'
 Write-Host
 Write-Host "[$scriptName] ---------- start ----------"
@@ -20,18 +22,48 @@ if ($imagePath) {
     exit 100
 }
 
-# Images shared via Vagrant cannot be mounted, so copy to image path
 $sourcePath = $args[1]
 if ($sourcePath) {
-
     Write-Host "[$scriptName] sourcePath : $sourcePath"
-    Write-Host
-	executeExpression "Copy-Item `"$sourcePath`" `"$imagePath`""
+} else {
+    Write-Host "[$scriptName] sourcePath not supplied, dismounting $imagePath"
+}
+
+$fallBack = $args[2]
+if ($fallBack) {
+    Write-Host "[$scriptName] fallBack   : $fallBack"
+} else {
+	$fallBack = 'c:\.provision'
+    Write-Host "[$scriptName] fallBack   : not supplied defaulting to $fallBack"
+}
+Write-Host
+
+# Obtain image and mount
+if ($sourcePath) {
+	if ($sourcePath -like 'http*') {	
+	    Write-Host "[$scriptName] Attempt download from web server $sourcePath"
+	    Write-Host
+		$filename = $sourcePath.Substring($sourcePath.LastIndexOf("/") + 1)
+		if ( Test-Path $imagePath ) {
+			Write-Host "[scriptName.ps1] $imagePath exists, download not required"
+		} else {
+		
+			$webclient = new-object system.net.webclient
+			Write-Host "[$scriptName] $webclient.DownloadFile($sourcePath, $imagePath)"
+			$webclient.DownloadFile($sourcePath, $imagePath)
+			# TODO: fallback if download fails
+		}
+	} else {
+	    Write-Host "[$scriptName] Attempt copy from file share $sourcePath"
+	    Write-Host
+		executeExpression "Copy-Item `"$sourcePath`" `"$imagePath`""
+	}
+
 	executeExpression "Mount-DiskImage -ImagePath `"$imagePath`""
 
+# Dismount image
 } else {
 
-    Write-Host "[$scriptName] sourcePath not supplied, dismounting $imagePath"
     Write-Host
 	executeExpression "Dismount-DiskImage -ImagePath `"$imagePath`""
 
