0286ebe73368ad7445184c328b515b015c813ff9
Jules Clements <cdaf@outlook.co.nz>
2017-03-14 10:18:38 +1300

    1.5.1.06 Add lastExitCode of 3010 as a success

--- automation/provisioning/IIS.ps1
+++ automation/provisioning/IIS.ps1
@@ -22,6 +22,7 @@ function executeRetry ($expression) {
 		    if(!$?) { Write-Host "[$scriptName] `$? = $?"; $exitCode = 1 }
 		} catch { echo $_.Exception|format-list -force; $exitCode = 2 }
 	    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; $exitCode = 3 }
+		if ( $LASTEXITCODE -eq 3010 ) { $LASTEXITCODE = 0 } # 3010 is a normal exit
 	    if ( $lastExitCode -ne 0 ) { Write-Host "[$scriptName] `$lastExitCode = $lastExitCode "; $exitCode = $lastExitCode }
 	    if ($exitCode -ne 0) {
 			if ($retryCount -ge $retryMax ) {
