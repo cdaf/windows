ae9275a5274522f1f7e799d5721fee0b826a5803
Jules Clements <cdaf@outlook.co.nz>
2017-03-26 10:20:57 +1300

    1.5.2.04 evaluate arguments for compres and decompress functions to support variables being passed.

--- automation/remote/execute.ps1
+++ automation/remote/execute.ps1
@@ -109,6 +109,7 @@ function ZipFiles( $zipfilename, $sourcedir )
 {
 	$currentDir = $(pwd)
 	$targetFile = "$currentDir\$zipfilename"
+	Write-Host "[$scriptName] DEBUG $targetFile"
 	Write-Host
 	Write-Host "[$scriptName] Create zip package $targetFile from $sourcedir"
 	$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
@@ -364,6 +365,7 @@ Foreach ($line in get-content $TASK_LIST) {
 	            if ( $feature -eq 'CMPRSS ' ) {
 		            Write-Host "$expression ==> " -NoNewline
 		            $arguments = $expression.Substring(7)
+		            $arguments = Invoke-Expression "Write-Output $arguments"
 					$data = $arguments.split(" ")
 					$filename = $data[0]
 					$source = $data[1]
@@ -376,6 +378,7 @@ Foreach ($line in get-content $TASK_LIST) {
 	            if ( $feature -eq 'DCMPRS ' ) {
 		            Write-Host "$expression ==> " -NoNewline
 		            $arguments = $expression.Substring(7)
+		            $arguments = Invoke-Expression "Write-Output $arguments"
 					$data = $arguments.split(" ")
 					$filename = $data[0]
 					$target = $data[1]
