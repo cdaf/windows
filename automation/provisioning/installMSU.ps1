8024e33b64cd28244f5a5a7082c83af7d84b0f9d
Jules Clements <cdaf@outlook.co.nz>
2017-05-03 14:08:56 +1200

    1.5.3.23 apply missing executeExpression function

--- automation/provisioning/installMSU.ps1
+++ automation/provisioning/installMSU.ps1
@@ -1,3 +1,17 @@
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$error.clear()
+	$LASTEXITCODE = 0
+	Write-Host "[$scriptName] $expression"
+	try {
+		$output = Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
+	} catch { echo $_.Exception|format-list -force; exit 2 }
+    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+    if ( $LASTEXITCODE -ne 0 ) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
+    return $output
+}
+
 $scriptName = 'installMSU.ps1'
 Write-Host "`n[$scriptName] Generic MSU installer"
 Write-Host "`n[$scriptName] ---------- start ----------"
