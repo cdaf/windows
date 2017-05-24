5ef30fbc3f575611edc73a63bb9653941bd070bb
Jules Clements <cdaf@outlook.co.nz>
2017-05-24 12:37:36 +1200

    1.5.4.10 Support for NuGet package download and placement

--- automation/provisioning/WebDeploy.ps1
+++ automation/provisioning/WebDeploy.ps1
@@ -1,3 +1,12 @@
+Param (
+  [string]$Installtype,
+  [string]$MsDepSvcPort,
+  [string]$version,
+  [string]$mediaDir
+)
+$scriptName = 'WebDeploy.ps1'
+$versionChoices = '2, 3.5 or 3.6' 
+
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	$error.clear()
@@ -6,16 +15,14 @@ function executeExpression ($expression) {
 		$output = Invoke-Expression $expression
 	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
 	} catch { echo $_.Exception|format-list -force; exit 2 }
+	if ( $LASTEXITCODE -ne 0 ) { exit $LASTEXITCODE }
     if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
     return $output
 }
 
-$scriptName = 'WebDeploy.ps1'
-$versionChoices = '2, 3.5 or 3.6' 
 Write-Host "`n[$scriptName] Install Web Deploy. As of Visual Studio 2015 Web Deploy build targets are automatically"
 Write-Host "[$scriptName] included, so the default action for this provisioner is now agent."
 Write-Host "`n[$scriptName] ---------- start ----------"
-$Installtype = $args[0]
 if ($Installtype) {
     Write-Host "[$scriptName] Installtype     : $Installtype"
 } else {
@@ -23,7 +30,6 @@ if ($Installtype) {
     Write-Host "[$scriptName] Installtype     : $Installtype (default, choices agent or build)"
 }
 
-$MsDepSvcPort = $args[1]
 if ($MsDepSvcPort) {
     Write-Host "[$scriptName] MsDepSvcPort    : $MsDepSvcPort"
 } else {
@@ -31,7 +37,6 @@ if ($MsDepSvcPort) {
     Write-Host "[$scriptName] MsDepSvcPort    : $MsDepSvcPort (default)"
 }
 
-$version = $args[2]
 if ($version) {
     Write-Host "[$scriptName] version         : $version"
 } else {
@@ -39,7 +44,6 @@ if ($version) {
     Write-Host "[$scriptName] version         : $version (default, choices $versionChoices)"
 }
 
-$mediaDir = $args[3]
 if ($mediaDir) {
     Write-Host "[$scriptName] mediaDir        : $mediaDir"
 } else {
@@ -50,12 +54,10 @@ if ($mediaDir) {
 if ( $env:PROV_SCRIPT_PATH ) {
 	Add-Content "$env:PROV_SCRIPT_PATH" "executeExpression `"./automation/provisioning/$scriptName $Installtype $MsDepSvcPort $version $mediaDir `""
 }
-
 if (!( Test-Path $mediaDir )) {
 	Write-Host "[$scriptName] mkdir $mediaDir"
 	mkdir $mediaDir
 }
-
 if ($env:interactive) {
     Write-Host "[$scriptName] env:interactive : $env:interactive, run in current window"
     $sessionControl = '-PassThru -Wait -NoNewWindow'
@@ -85,66 +87,76 @@ switch ($version) {
     }
 }
 
-# Prepare Install Media
-$installFile = $mediaDir + '\' + $file
-Write-Host "[$scriptName] installFile     : $installFile"
-
-$logFile = $installDir = [Environment]::GetEnvironmentVariable('TEMP', 'user') + '\' + $file + '.log'
-Write-Host "[$scriptName] logFile         : $logFile"
-
-Write-Host
-$fullpath = $mediaDir + '\' + $file
-if ( Test-Path $fullpath ) {
-	Write-Host "[$scriptName] $fullpath exists, download not required"
-} else {
-	$webclient = new-object system.net.webclient
-	Write-Host "[$scriptName] $webclient.DownloadFile(`"$uri`", `"$fullpath`")"
-	$webclient.DownloadFile($uri, $fullpath)
-}
-
-# Output File (plain text or XML depending on method) must be supplioed
-if ($Installtype -eq 'agent') {
-    Write-Host "[$scriptName] For Installtype $Installtype, bind listener with default setting"
-	$argList = @(
-		"/qn",
-		"/norestart",
-		"LicenseAccepted=`"0`"",
-		"/L*V", # Log Verbose
-		"$logFile",
-		"/i", # Install file path
-		"$installFile",
-		"ADDLOCAL=ALL",
-		"LISTENURL=http://+:${MsDepSvcPort}/MsDeployAgentService"
-	)
-} else {
-	
-    Write-Host "[$scriptName] For Installtype $Installtype, only deploy MSBuild targets"
-	$argList = @(
-		"/qn",
-		"/L*V",
-		"$logFile",
-		"/i",
-		"$installFile"
-	)
-}
-
-# Perform Install
-executeExpression "`$process = Start-Process -FilePath `'msiexec`' -ArgumentList `'$argList`' $sessionControl"
-
 $key = "HKLM:\SOFTWARE\Microsoft\IIS Extensions\MSDeploy\$installPath"
 $name = 'InstallPath'
 
 # Retrieve the install path from the registry key, if missing, halt with error
-$InstallPath = executeExpression "(Get-ItemProperty -Path `'$key`' -Name $name).$name"
-
-write-host "Set environment variable MSDeployPath to $InstallPath"
-executeExpression "[Environment]::SetEnvironmentVariable('MSDeployPath', `'$InstallPath`', `'Machine`')"
-
-$failed = Select-String $logFile -Pattern "Installation failed"
-if ( $failed  ) { 
-	Select-String $logFile -Pattern "Installation success or error status"
-	exit 4
+$InstallPath = (Get-ItemProperty -Path "$key" -Name $name).$name
+if ( $InstallPath ) {
+	Write-Host "[$scriptName] Web Deploy already installed, no action attempted."
+} else {
+	
+	# Prepare Install Media
+	$installFile = $mediaDir + '\' + $file
+	Write-Host "[$scriptName] installFile     : $installFile"
+	
+	$logFile = $installDir = [Environment]::GetEnvironmentVariable('TEMP', 'user') + '\' + $file + '.log'
+	Write-Host "[$scriptName] logFile         : $logFile"
+	
+	Write-Host
+	$fullpath = $mediaDir + '\' + $file
+	if ( Test-Path $fullpath ) {
+		Write-Host "[$scriptName] $fullpath exists, download not required"
+	} else {
+		$webclient = new-object system.net.webclient
+		Write-Host "[$scriptName] $webclient.DownloadFile(`"$uri`", `"$fullpath`")"
+		$webclient.DownloadFile($uri, $fullpath)
+	}
+	
+	# Output File (plain text or XML depending on method) must be supplioed
+	if ($Installtype -eq 'agent') {
+	    Write-Host "[$scriptName] For Installtype $Installtype, bind listener with default setting"
+		$argList = @(
+			"/qn",
+			"/norestart",
+			"LicenseAccepted=`"0`"",
+			"/L*V", # Log Verbose
+			"$logFile",
+			"/i", # Install file path
+			"$installFile",
+			"ADDLOCAL=ALL",
+			"LISTENURL=http://+:${MsDepSvcPort}/MsDeployAgentService"
+		)
+	} else {
+		
+	    Write-Host "[$scriptName] For Installtype $Installtype, only deploy MSBuild targets"
+		$argList = @(
+			"/qn",
+			"/L*V",
+			"$logFile",
+			"/i",
+			"$installFile"
+		)
+	}
+	
+	# Perform Install
+	$proc = executeExpression "Start-Process -FilePath `'msiexec`' -ArgumentList `'$argList`' $sessionControl"
+	if ( $proc.ExitCode -ne 0 ) {
+		Write-Host "`n[$scriptName] Install Failed, see log file (c:\windows\logs\CBS\CBS.log) for details. Exit with `$LASTEXITCODE $($proc.ExitCode)`n"
+	    exit $proc.ExitCode
+	}
+	
+	# Retrieve the install path from the registry key, if missing, halt with error
+	$InstallPath = executeExpression "(Get-ItemProperty -Path `'$key`' -Name $name).$name"
+	
+	write-host "Set environment variable MSDeployPath to $InstallPath"
+	executeExpression "[Environment]::SetEnvironmentVariable('MSDeployPath', `'$InstallPath`', `'Machine`')"
+	
+	$failed = Select-String $logFile -Pattern "Installation failed"
+	if ( $failed  ) { 
+		Select-String $logFile -Pattern "Installation success or error status"
+		exit 4
+	}
 }
 
 Write-Host "`n[$scriptName] ---------- stop ----------"
-exit 0
---
+++ automation/provisioning/nuget.ps1
@@ -0,0 +1,88 @@
+Param (
+  [string]$packageName,
+  [string]$targetDirectory,
+  [string]$subDirectory,
+  [string]$version
+)
+$scriptName = 'nuget.ps1'
+
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$error.clear()
+	Write-Host "[$scriptName] $expression"
+	try {
+		$output = Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
+	} catch { echo $_.Exception|format-list -force; exit 2 }
+    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+    return $output
+}
+
+# NuGet install is developer oriented for including packages into Visual Studio ecosystem, this script provides a wrapper for downloading a package and copying it to a destination, if the destination does not exist, it will be created.
+Write-Host "`n[$scriptName] ---------- start ----------"
+if ($packageName) {
+    Write-Host "[$scriptName] packageName     : $packageName"
+} else {
+    Write-Host "[$scriptName] packageName not supplied, exiting with LASTEXITCODE 101" exit 101
+}
+
+if ($targetDirectory) {
+    Write-Host "[$scriptName] targetDirectory : $targetDirectory"
+} else {
+    Write-Host "[$scriptName] targetDirectory not supplied, exiting with LASTEXITCODE 102" exit 102
+}
+
+if ($version) {
+    Write-Host "[$scriptName] version         : $version"
+    $optArg += " -version $version"
+} else {
+    Write-Host "[$scriptName] version         : ( not supplied )"
+}
+
+if ($subDirectory) {
+    Write-Host "[$scriptName] subDirectory    : $subDirectory"
+    $optArg += " -subDirectory $subDirectory"
+} else {
+    Write-Host "[$scriptName] subDirectory    : ( not supplied )"
+}
+
+# Provisioning Script builder
+if ( $env:PROV_SCRIPT_PATH ) {
+	Add-Content "$env:PROV_SCRIPT_PATH" "executeExpression `'./automation/provisioning/$scriptName $packageName `"$targetDirectory`"$optArg`'"
+}
+$nugetOutDir = "$env:temp\packages"
+if ( Test-Path $nugetOutDir ) {
+	Write-Host "[$scriptName] $nugetOutDir exists, this is the cache NuGet will use"
+} else {
+	$newDir = executeExpression "New-Item -ItemType Directory -Force -Path `'$nugetOutDir`'"
+	Write-Host "Created $($newDir.FullName)"
+}
+
+if ( Test-Path $targetDirectory ) {
+	Write-Host "[$scriptName] $targetDirectory exists, if files exist they will be replaced"
+} else {
+	$newDir = executeExpression "New-Item -ItemType Directory -Force -Path `'$targetDirectory`'"
+	Write-Host "Created $($newDir.FullName)"
+}
+
+if ($version) {
+	executeExpression "nuget install $packageName -Version $version -OutputDirectory $nugetOutDir"
+} else {
+	executeExpression "nuget install $packageName -OutputDirectory $nugetOutDir"
+}
+
+$files = Get-ChildItem "$nugetOutDir\$packageName*"
+if ($files) {
+	Write-Host;	Write-Host "[$scriptName] Packages available ..."
+	foreach ($file in $files) {
+		Write-Host "[$scriptName]   $($file.name)"
+		$packageDirectory = $($file.fullname)
+	}
+	Write-Host; Write-Host "[$scriptName] Using latest package ($packageDirectory)"
+} else {
+	Write-Host "[$scriptName] packageDirectory with prefix `'$packageName`' not found, exiting with error code 1"; exit 1
+}
+
+executeExpression "Copy-Item -Recurse -Force `"$packageDirectory\$subDirectory\**`" `"$targetDirectory`""
+
+Write-Host "`n[$scriptName] ---------- stop ----------`n"
