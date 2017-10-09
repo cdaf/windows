43413b1a5840cfc4c549dd8dcca14cf88564c782
Jules <cdaf@outlook.co.nz>
2017-10-10 08:41:24 +1300

    Remove LASTEXITCODE initalisation as causes false positives.

--- automation/remote/containerBuild.ps1
+++ automation/remote/containerBuild.ps1
@@ -10,14 +10,13 @@ $scriptName = 'containerBuild.ps1'
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	$error.clear()
-	$LASTEXITCODE = 0
 	Write-Host "[$scriptName] $expression"
 	try {
 		Invoke-Expression $expression
 	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
 	} catch { echo $_.Exception|format-list -force; exit 2 }
-    if ( $LASTEXITCODE -ne 0 ) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
     if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+    if ( $LASTEXITCODE -ne 0 ) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
 }
 
 # Use the CDAF provisioning helpers
