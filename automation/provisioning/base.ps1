9f8ef6deb6fe51f28251f6efd8c5c67ac83fc300
Jules <cdaf@outlook.co.nz>
2018-01-18 14:11:16 +1300

    1.6.4.06 install Chocolatey if not currently installed before attempting to install selected software.

--- automation/provisioning/base.ps1
+++ automation/provisioning/base.ps1
@@ -18,10 +18,58 @@ $scriptName = 'base.ps1'
 Write-Host "[$scriptName] Install components using Chocolatey.`n"
 Write-Host "[$scriptName] ---------- start ----------"
 if ($install) {
-    Write-Host "[$scriptName] install : $install"
+    Write-Host "[$scriptName] install    : $install"
 } else {
     Write-Host "[$scriptName] Package to install not supplied, exiting with LASTEXITCODE 4"; exit 4 
 }
+
+if ($mediaDir) {
+    Write-Host "[$scriptName] mediaDir   : $mediaDir"
+} else {
+	$mediaDir = '/.provision'
+    Write-Host "[$scriptName] mediaDir   : $mediaDir (default)"
+}
+
+$versionTest = cmd /c choco --version 2`>`&1
+if ($versionTest -like '*not recognized*') {
+	Write-Host "`n[$scriptName] Chocolatey not installed, installing ..."
+	cmd /c "exit 0"
+	if (!( Test-Path $mediaDir )) {
+		Write-Host "[$scriptName] mkdir $mediaDir"
+		Write-Host "[$scriptName]   $(mkdir $mediaDir) created"
+	}
+	
+	Write-Host
+	$file = 'install.ps1'
+	$fullpath = $mediaDir + '\' + $file
+	if ( Test-Path $fullpath ) {
+		Write-Host "[$scriptName] $fullpath exists, download not required"
+	} else {
+	
+		$uri = 'https://chocolatey.org/' + $file
+		executeExpression "(New-Object System.Net.WebClient).DownloadFile(`"$uri`", `"$fullpath`")"
+	}
+	
+	try {
+		$argList = @("$fullpath")
+		Write-Host "[$scriptName] Start-Process -FilePath 'powershell' -ArgumentList $argList -PassThru -Wait"
+		$proc = Start-Process -FilePath 'powershell' -ArgumentList $argList -PassThru -Wait
+	} catch {
+		Write-Host "[$scriptName] $file Install Exception : $_" -ForegroundColor Red
+		exit 200
+	}
+	
+	# Reload the path (without logging off and back on)
+	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+	$versionTest = cmd /c choco --version 2`>`&1
+	if ($versionTest -like '*not recognized*') {
+		Write-Host "[$scriptName] Chocolatey install has failed, exiting!"
+		exit # that should retain the exit code from the version test itself
+	}
+
+}
+Write-Host "[$scriptName] Chocolatey : $versionTest"
 
 Write-Host
 executeExpression "choco install -y $install --no-progress --fail-on-standard-error"
