cdc060a3ec147f1dfc6af0f0a08096ce99f82a96
Jules Clements <julesc@datacom.co.nz>
2018-02-02 11:36:20 +1300

    1.6.6.07 work in progress dotnet core installer, currently SDK only

---
+++ automation/provisioning/installDotnetCore.ps1
@@ -0,0 +1,61 @@
+Param (
+	[string]$sdk,
+	[string]$version,
+	[string]$mediaDir
+)
+$scriptName = 'installDotnetCore.ps1'
+
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$error.clear()
+	Write-Host "[$scriptName] $expression"
+	try {
+		$output = Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
+	} catch { echo $_.Exception|format-list -force; exit 2 }
+    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+    if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
+    return $output
+}
+
+cmd /c "exit 0"
+Write-Host "`n[$scriptName] ---------- start ----------"
+if ( $sdk ) {
+	Write-Host "[$scriptName] sdk      : $sdk (choices yes or no)"
+} else {
+	$sdk = 'no'
+	Write-Host "[$scriptName] sdk      : $sdk (default, choices yes or no)"
+}
+
+if ( $version ) {
+	Write-Host "[$scriptName] version  : $version"
+} else {
+	$version = '2.1.4'
+	Write-Host "[$scriptName] version  : $version (default)"
+}
+
+if ( $mediaDir ) {
+	Write-Host "[$scriptName] mediaDir : $mediaDir`n"
+} else {
+	$mediaDir = 'C:\.provision'
+	Write-Host "[$scriptName] mediaDir : $mediaDir (not passed, set to default)`n"
+}
+
+if ( $sdk -eq 'yes' ) {
+	$installer = "${mediaDir}\dotnet-sdk-${$version}-win-x64.exe"
+	if ( Test-Path $installer ) {
+		Write-Host "[$scriptName] Installer $installer found, download not required`n"
+	} else {
+		Write-Host "[$scriptName] Installer $installer not found, attempting download`n"
+		executeExpression "(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/1/1/5/115B762D-2B41-4AF3-9A63-92D9680B9409/dotnet-sdk-2.1.4-win-x64.exe', '$installer')"
+	}
+	
+	$proc = executeExpression "Start-Process -FilePath '$installer' -ArgumentList '/INSTALL /QUIET /NORESTART /LOG $installer.log' -PassThru -Wait"
+	if ( $proc.ExitCode -ne 0 ) {
+		Write-Host "`n[$scriptName] Exit with `$LASTEXITCODE = $($proc.ExitCode)`n"
+	    exit $proc.ExitCode
+	}
+}
+
+Write-Host "`n[$scriptName] ---------- stop -----------"
+exit 0
