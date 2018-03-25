5f24289448c66ae2ba8db8a4f845f01cad2bd74f
Jules <cdaf@outlook.co.nz>
2018-03-26 09:22:32 +1300

    VirtualBox additions verted from 5.1.30 to 5.1.10 to try and fix boot issue with 2016 GUI (aligns with Linux)

--- AtlasImage.ps1
+++ AtlasImage.ps1
@@ -88,13 +88,13 @@ if ($stripDISM) {
 }
 	
 if ( $hypervisor -eq 'virtualbox' ) {
-	executeExpression ".\automation\provisioning\mountImage.ps1 $env:userprofile\VBoxGuestAdditions_5.1.30.iso http://download.virtualbox.org/virtualbox/5.1.30/VBoxGuestAdditions_5.1.30.iso"
+	executeExpression ".\automation\provisioning\mountImage.ps1 $env:userprofile\VBoxGuestAdditions_5.1.10.iso http://download.virtualbox.org/virtualbox/5.1.10/VBoxGuestAdditions_5.1.10.iso"
 	$result = executeExpression "[Environment]::GetEnvironmentVariable(`'MOUNT_DRIVE_LETTER`', `'User`')"
 	emailProgress "Guest Additiions requires manual intervention ..."
 	
 	executeExpression "`$proc = Start-Process -FilePath `"$result\VBoxWindowsAdditions-amd64.exe`" -ArgumentList `'/S`' -PassThru -Wait"
-	executeExpression ".\automation\provisioning\mountImage.ps1 $env:userprofile\VBoxGuestAdditions_5.1.30.iso"
-	executeExpression "Remove-Item $env:userprofile\VBoxGuestAdditions_5.1.30.iso"
+	executeExpression ".\automation\provisioning\mountImage.ps1 $env:userprofile\VBoxGuestAdditions_5.1.10.iso"
+	executeExpression "Remove-Item $env:userprofile\VBoxGuestAdditions_5.1.10.iso"
 } else {
 	writeLog "Hypervisor ($hypervisor) not virtualbox, skip Guest Additions install"
 }
