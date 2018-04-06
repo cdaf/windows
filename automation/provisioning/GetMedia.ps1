fb9b4d2d3de959bae606ca37f8e12a80b3144f6c
Jules <cdaf@outlook.co.nz>
2018-04-06 13:49:49 +1200

    Replace GitHub switch for TLS1.1 and above to fail/retry logic

--- automation/provisioning/GetMedia.ps1
+++ automation/provisioning/GetMedia.ps1
@@ -5,6 +5,40 @@ Param (
 )
 $scriptName = 'GetMedia.ps1'
 
+# Retry logic for connection issues, i.e. "Cannot retrieve the dynamic parameters for the cmdlet. PowerShell Gallery is currently unavailable.  Please try again later."
+# Includes warning for "Cannot find a variable with the name 'PackageManagementProvider'. Cannot find a variable with the name 'SourceLocation'."
+function executeRetry ($expression) {
+	$exitCode = 1
+	$wait = 10
+	$retryMax = 3
+	$retryCount = 0
+	while (( $retryCount -le $retryMax ) -and ($exitCode -ne 0)) {
+		$exitCode = 0
+		$error.clear()
+		Write-Host "[$retryCount] $expression"
+		try {
+			Invoke-Expression $expression
+		    if(!$?) { Write-Host "[$scriptName] `$? = $?" -ForegroundColor Red; $exitCode = 1 }
+		} catch { Write-Host "[$scriptName] $_" -ForegroundColor Red; $exitCode = 2 }
+	    if ( $error[0] ) { Write-Host "[$scriptName] Warning, message in `$error[0] = $error" -ForegroundColor Yellow; $error.clear() } # do not treat messages in error array as failure
+	    if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$lastExitCode = $lastExitCode " -ForegroundColor Red; $exitCode = $lastExitCode }
+	    if ($exitCode -ne 0) {
+			if ($retryCount -ge $retryMax ) {
+				Write-Host "[$scriptName] Retry maximum ($retryCount) reached, exiting with `$LASTEXITCODE = $exitCode.`n"
+				exit $exitCode
+			} else {
+				$retryCount += 1
+				Write-Host "[$scriptName] Set TLS to version 1.1 or higher, Wait $wait seconds, then retry $retryCount of $retryMax"
+				Write-Host "`$AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'"
+				$AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+				executeExpression '[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols'
+				sleep $wait
+				$wait = $wait + $wait
+			}
+		}
+    }
+}
+
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	$error.clear()
@@ -55,13 +89,6 @@ if ( Test-Path $mediaDir ) {
 	Write-Host "[$scriptName] Created $(mkdir $mediaDir)"
 }
 
-if ($uri -match 'github.com') {
-	Write-Host "[$scriptName] URL contains github.com, set TLS to version 1.1 or higher"
-	Write-Host "`$AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'"
-	$AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
-	executeExpression '[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols'
-}
-
 $file = $uri.Substring($uri.LastIndexOf("/") + 1)
 $fullpath = $mediaDir + '\' + $file
 if ( Test-Path $fullpath ) {
@@ -74,7 +101,7 @@ if ( Test-Path $fullpath ) {
 	} catch { $fullpath = listAndContinue }
 
 	Write-Host "[$scriptName] Attempt download"
-	executeExpression "(New-Object System.Net.WebClient).DownloadFile('$uri', '$fullpath')"
+	executeRetry "(New-Object System.Net.WebClient).DownloadFile('$uri', '$fullpath')"
 }
 
 if ( $md5 ) {
