ba73a483f9c741159c8a8536b6440382f497a34a
Jules <cdaf@outlook.co.nz>
2018-08-18 08:40:36 +1200

    1.7.4.01 CDAF upgrade script

---
+++ automation/upgrade/cdafUpgrade.ps1
@@ -0,0 +1,33 @@
+$scriptName = 'cdafUpgrade.ps1'
+cmd /c "exit 0"
+
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$error.clear()
+	Write-Host "$expression"
+	try {
+		Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
+	} catch { echo $_.Exception|format-list -force; exit 2 }
+    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+    if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
+}
+
+Write-Host "`n[$scriptName] ---------- start ----------"
+
+$zipFile = 'LU-CDAF.zip'
+$url = "http://cdaf.io/static/app/downloads/$zipFile"
+$extract = "$env:TEMP\LU-CDAF"
+if (Test-Path $extract ) {
+	executeExpression "Remove-Item -Recurse -Force $extract"
+}
+executeExpression "mkdir $extract"
+executeExpression "(New-Object System.Net.WebClient).DownloadFile('$url', '$extract\$zipfile')"
+executeExpression "Add-Type -AssemblyName System.IO.Compression.FileSystem"
+executeExpression "[System.IO.Compression.ZipFile]::ExtractToDirectory('$extract\$zipfile', '$extract')"
+ 
+executeExpression 'Remove-Item -Recurse .\automation\'
+executeExpression 'Copy-Item -Recurse $extract\automation .'
+
+Write-Host "`n[$scriptName] ---------- stop ----------"
+exit 0
