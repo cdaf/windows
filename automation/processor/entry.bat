42a326480a99420021208b3dd7e00e22bc67439e
Jules <cdaf@outlook.co.nz>
2019-01-24 13:22:58 +1300

    execute delivery in-line to avoid false possitive

--- automation/processor/entry.bat
+++ automation/processor/entry.bat
@@ -1,41 +1,48 @@
-@echo off
+@ECHO OFF
 cmd /c "exit 0"
 
-rem Too complicated to derive automation root from Batch runner, so hard coded for windows.
-set BUILDNUMBER=%1
-set BRANCH=%2
-set ACTION=%3
+REM Too complicated to derive automation root from Batch runner, so hard coded for windows.
+SET BUILDNUMBER=%1
+SET BRANCH=%2
+SET ACTION=%3
 
 echo.
 echo [%~nx0] --------------------
 echo [%~nx0] Targetless Branch CD
-echo [%~nx0] --------------------
+echo [%~nx0]   BUILDNUMBER : %BUILDNUMBER%
+echo [%~nx0]   BRANCH      : %BRANCH%
+echo [%~nx0]   ACTION      : %ACTION%
 
-rem Launcher script that overides execution policy
-rem cannot elevate powershell
+REM Launcher script that overides execution policy
+REM cannot elevate powershell
 
-call powershell -NoProfile -ExecutionPolicy ByPass -command %cd%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH% %ACTION%
-set result=%errorlevel%
+call %CD%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH% %ACTION%
+SET result=%ERRORLEVEL%
 if %result% NEQ 0 (
-	echo [%~nx0] ERROR call powershell -NoProfile -ExecutionPolicy ByPass -command %cd%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH% %ACTION%
+	echo [%~nx0] ERROR call %CD%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH% %ACTION%
 	echo [%~nx0]   Return LASTEXITCODE %result% 
 	echo.
-    echo [%~nx0] --- End Emulation Error Handling ---
+    echo [%~nx0] --- End Targetless Branch CI Error Handling ---
 	echo.
 	exit /b %result%
 )
 
-IF %BRANCH% NEQ master (
-	call powershell -NoProfile -ExecutionPolicy ByPass -command %cd%\TasksLocal\delivery.bat DOCKER
-	set result=%errorlevel%
-	if %result% NEQ 0 (
-		echo [%~nx0] ERROR call powershell -NoProfile -ExecutionPolicy ByPass -command %cd%\TasksLocal\delivery.bat DOCKER
-		echo [%~nx0]   Return LASTEXITCODE %result% 
-		echo.
-	    echo [%~nx0] --- End Emulation Error Handling ---
-		echo.
-		exit /b %result%
-	)
-) ELSE (
+IF "%BRANCH%" == "master" (
 	echo [%~nx0] Only perform container test in CI for branches, Master execution in CD pipeline
+	exit /b 0
+) ELSE (
+	GOTO :EXITNEST
+)
+
+# REM Do not call from within IF statement or errorlevel is lost
+:EXITNEST
+call %CD%\TasksLocal\delivery.bat DOCKER
+SET result=%ERRORLEVEL%
+if %result% NEQ 0 (
+	echo [%~nx0] ERROR call %CD%\TasksLocal\delivery.bat DOCKER
+	echo [%~nx0]   Return LASTEXITCODE %result% 
+	echo.
+    echo [%~nx0] --- End Targetless Branch CD Error Handling ---
+	echo.
+	exit /b %result%
 )
