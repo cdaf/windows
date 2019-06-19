750b527d91ae2cb3b7f00e88eeae372baa9ca90e
Jules <cdaf@outlook.co.nz>
2019-06-19 13:47:29 +1200

    1.9.2.04 only pass action for master branch (suppress staging artefact)

--- automation/processor/entry.bat
+++ automation/processor/entry.bat
@@ -1,21 +1,39 @@
 @ECHO OFF
 cmd /c "exit 0"
 
-REM Too complicated to derive automation root from Batch runner, so hard coded for windows.
+REM Action can be used to pass staging directory with staging@ prefix
 SET BUILDNUMBER=%1
 SET BRANCH=%2
-SET URL=%3
+SET ACTION=%3
 
 echo.
 echo [%~nx0] --------------------
 echo [%~nx0] Targetless Branch CD
 echo [%~nx0]   BUILDNUMBER : %BUILDNUMBER%
 echo [%~nx0]   BRANCH      : %BRANCH%
-echo [%~nx0]   URL         : %URL%
+echo [%~nx0]   ACTION      : %ACTION%
 
 REM Launcher script that overides execution policy
 REM cannot elevate powershell
 
+IF "%BRANCH%" == "master" (
+	GOTO StagingArtefacts
+)
+
+echo [%~nx0] Do not pass ACTION when executing non-master branch
+call %CD%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH%
+SET result=%ERRORLEVEL%
+if %result% NEQ 0 (
+	echo [%~nx0] ERROR call %CD%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH%
+	echo [%~nx0]   Return LASTEXITCODE %result% 
+	echo.
+    echo [%~nx0] --- End Targetless Branch CI Error Handling ---
+	echo.
+	exit /b %result%
+)
+GOTO BranchCheck
+
+:StagingArtefacts
 call %CD%\automation\processor\buildPackage.bat %BUILDNUMBER% %BRANCH% %ACTION%
 SET result=%ERRORLEVEL%
 if %result% NEQ 0 (
@@ -27,6 +45,7 @@ if %result% NEQ 0 (
 	exit /b %result%
 )
 
+:BranchCheck
 IF "%BRANCH%" == "master" (
 	echo [%~nx0] Only perform container test in CI for branches, Master execution in CD pipeline
 	GOTO BranchImageTest
@@ -55,10 +74,10 @@ IF "%BRANCH_IMAGE_CLEAN%" == "" GOTO endOfScript
 if NOT %BRANCH_IMAGE_CLEAN% == "yes" GOTO endOfScript
 
 :GitClean
-call powershell -NoProfile -NonInteractive -ExecutionPolicy ByPass -command %cd%\%automationRoot%\processor\removeDockerImage.ps1 %URL%
+call powershell -NoProfile -NonInteractive -ExecutionPolicy ByPass -command %cd%\%automationRoot%\processor\removeDockerImage.ps1 %ACTION%
 set result=%errorlevel%
 if %result% NEQ 0 (
-	echo [%~nx0] BUILD_PACKAGE_ERROR call powershell -NoProfile -NonInteractive -ExecutionPolicy ByPass -command %cd%\%automationRoot%\processor\removeDockerImage.ps1 %URL%
+	echo [%~nx0] BUILD_PACKAGE_ERROR call powershell -NoProfile -NonInteractive -ExecutionPolicy ByPass -command %cd%\%automationRoot%\processor\removeDockerImage.ps1 %ACTION%
 	echo [%~nx0]   Return LASTEXITCODE %result% 
 	exit /b %result%
 )
