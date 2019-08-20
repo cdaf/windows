a8071d08aa4ad91d0e50e351e71a3f3b637efed3
Jules <cdaf@outlook.co.nz>
2019-08-20 14:55:42 +1200

    WIP encrypt with decrypt

--- automation/provisioning/encypt.ps1
+++ automation/provisioning/encypt.ps1
@@ -1,7 +1,7 @@
 Param (
-	[string]$aesKey,
 	[string]$inputFile,
-	[string]$encyptedFile
+	[string]$encryptedFile,
+	[string]$stringKeyIn
 )
 
 cmd /c "exit 0"
@@ -21,52 +21,35 @@ function taskComplete { param ($taskName)
 
 $scriptName = 'encrypt.ps1'
 
-Write-Host; Write-Host "[$scriptName] Create an encrypted file using AES key "; Write-Host
-Write-Host "[$scriptName] ---------- start ----------"
-if ($aesKey) {
-    Write-Host "[$scriptName] aesKey : $aesKey"
+Write-Host "`n[$scriptName] Create an encrypted file using AES key "
+Write-Host "`n[$scriptName] ---------- start ----------"
+if ($inputFile) {
+    Write-Host "[$scriptName] inputFile     : $inputFile"
 } else {
-    Write-Host "[$scriptName] aesKey : (not supplied, new key will be generated"
+    Write-Host "[$scriptName] inputFile not suppplied!"; exit 101
 }
 
-if ($KeyFile) {
-    Write-Host "[$scriptName] KeyFile  : $targetHost"
+if ($encryptedFile) {
+    Write-Host "[$scriptName] encryptedFile : $encryptedFile"
 } else {
-    Write-Host "[$scriptName] KeyFile  : (not suppplied, key will be output to standard out)"
+    Write-Host "[$scriptName] encryptedFile : (not suppplied, existing file will be replaced)"
 }
 
-Write-Host "[$scriptName] Creating AES key with random data"
-$Key = New-Object Byte[] 16   # You can use 16, 24, or 32 for AES
-[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
-
-if ($KeyFile) {
-	$Key | out-file $KeyFile
+if ($stringKeyIn) {
+    Write-Host "[$scriptName] stringKeyIn   : `$stringKeyIn (example format 29-240-88-121-33-64-150-226-136-160-144-115-127-74-74-30)"
+    $key = @()
+    $key = $stringKeyIn.Split('-')
+} else {
+    Write-Host "[$scriptName] stringKeyIn   : (not supplied, new key will be generated, example format 29-240-88-121-33-64-150-226-136-160-144-115-127-74-74-30)"
+	$Key = New-Object Byte[] 16   # You can use 16, 24, or 32 for AES
+	[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+	$stringKeyOut = ""
+	foreach ($element in $key) { $stringKeyOut += "-$element" }
+	$stringKeyOut = $stringKeyOut.Substring(1)
 }
 
-Create a SecureString from encrypted password using the Key that encrypted it
-byte[]]$Key = (52,114,35,179,55,90,163,246,117,161,195,233,75,138,8,127)
-
-$stringKey = ""
-foreach ($element in $key) { $stringKey += "-$element" }
-$stringKey = $stringKey.Substring(1)
-
-$encryptedFile = 
-Get-Content .\build.tsk -Raw  | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
-
-# Creating SecureString object and export SecureString as encrypted string to file
-$Key = Get-Content $KeyFile
-$Password = "Ducati906&" | ConvertTo-SecureString -AsPlainText -Force 
-$PasswordFile = "C:\scripts\Password.txt"
-$Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
-$PasswordSecureString = (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)
-
-# Use file contents as encrypted password and convert to SecureString using the Key that encrypted it.
-Password = '76492d1116743f0423413b16050a5345MgB8AG4AMABwAG4AYQByAFAAdQBSADAAYwAwAHAASAB5AGIATgBkAFIATgBDAGcAPQA9AHwAMABlAGYANgBmADQAYgAzAGQAMQAyADEAZgBmADQAZABhADIAYwA2AGQAMAA1AGQAMQBhAGQANAAzADkAYQA4AGUAOAA3ADMANwA3ADAANQA1ADEAMQA3ADkAYwA1AGYAOABkADIANAA1AGMAZABiAGYAYQA5ADQAZQAxADAANQA='
-PasswordSecureString = $Password | ConvertTo-SecureString -Key $key
-
-#Creating PSCredential object
-$Username = "cicd-tnz"
-$Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $PasswordSecureString
-
-Write-Host
-Write-Host "[$scriptName] ---------- stop ----------"
+Get-Content $inputFile -Raw  | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -key $Key | Out-File $encryptedFile
+if ( $stringKeyOut ) {
+	$stringKeyOut
+}
+Write-Host "`n[$scriptName] ---------- stop ----------"
--- automation/remote/decryptKey.ps1
+++ automation/remote/decryptKey.ps1
@@ -39,53 +39,68 @@ if (! (Test-Path $encryptedFile) ) {
 	}
 }
 
-# If a thumbprint is passed, decrypt file using PKI
-if ($thumbprint) {
+# If a thumbprint is passed, check for AES key
+if ( $thumbprint ) {
 
-	try {
-		# Check for certificate existance
-		if (! $location) {
-		    $location = 'LocalMachine'
-		}
-		if (!( Test-Path "Cert:\$location\My\$thumbprint" )) {
-			if ($location -eq 'LocalMachine') {
-			    $location = 'CurrentUser'
-		    } else {
+	if ( $thumbprint -match '-' ) {
+		Write-Host "[$scriptName] Processing thumbprint as AES key"
+	    $key = @()
+	    $key = $stringKeyIn.Split('-')
+	    $secureFileInMemory = Get-Content $PasswordFile | ConvertTo-SecureString -Key $key
+	    if ( $location ) {
+			Write-Host "[$scriptName] Using location for decrypted file"
+			[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureFileInMemory)) | $location
+	    } else {
+			Write-Host "[$scriptName] Location not passed so overwriting input file"
+		    [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureFileInMemory)) | $encryptedFile
+	    }
+	} else {
+		Write-Host "[$scriptName] Decrypt file using PKI"
+		try {
+			# Check for certificate existance
+			if (! $location) {
 			    $location = 'LocalMachine'
 			}
 			if (!( Test-Path "Cert:\$location\My\$thumbprint" )) {
-			    throwError "DECRYPT_KEY_107" "Unable to find thumbprint in either Cert:\CurrentUser\My\$thumbprint or Cert:\LocalMachine\My\$thumbprint."
-		    }
+				if ($location -eq 'LocalMachine') {
+				    $location = 'CurrentUser'
+			    } else {
+				    $location = 'LocalMachine'
+				}
+				if (!( Test-Path "Cert:\$location\My\$thumbprint" )) {
+				    throwError "DECRYPT_KEY_107" "Unable to find thumbprint in either Cert:\CurrentUser\My\$thumbprint or Cert:\LocalMachine\My\$thumbprint."
+			    }
+			}
+	
+			# Attempted to open and decrypt
+		    $object = Import-Clixml -Path $encryptedFile
+			if (! $object) {
+			    throwError "DECRYPT_KEY_102" "Unable to Import-Clixml $encryptedFile! Exit Code 102."
+			}
+			if (! $object.Key) {
+			    throwError "DECRYPT_KEY_105" "Cannot retrieve private key for $encryptedFile! Exit Code 105."
+			}
+		    $cert = Get-Item -Path "Cert:\$location\My\$thumbprint" -ErrorAction Stop
+			if (! $cert) {
+			    throwError "DECRYPT_KEY_103" "Unable to open certificate Cert:\$location\My\$thumbprint"
+			}
+			if (! $cert.PrivateKey) {
+				Write-Host "[$scriptName] Are you running as administrator? Elevation is required to retrieve private key!"
+			    throwError "DECRYPT_KEY_106" "Unable to open private key for certificate Cert:\$location\My\$thumbprint"
+			}
+			# Write-Host "[$scriptName] `$object = $object"
+		    $key = $cert.PrivateKey.Decrypt($object.Key, $true)
+			if (! $cert) {
+			    throwError "DECRYPT_KEY_104" "Unable to decrypt using private key."
+			}
+			# Write-Host "[$scriptName] `$key = $key"
+		    $retrievedString = $object.Payload | ConvertTo-SecureString -Key $key
+			$plain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($retrievedString))
+		} catch {
+			throw
+		} finally {
+		    if ($null -ne $key) { [array]::Clear($key, 0, $key.Length) }
 		}
-
-		# Attempted to open and decrypt
-	    $object = Import-Clixml -Path $encryptedFile
-		if (! $object) {
-		    throwError "DECRYPT_KEY_102" "Unable to Import-Clixml $encryptedFile! Exit Code 102."
-		}
-		if (! $object.Key) {
-		    throwError "DECRYPT_KEY_105" "Cannot retrieve private key for $encryptedFile! Exit Code 105."
-		}
-	    $cert = Get-Item -Path "Cert:\$location\My\$thumbprint" -ErrorAction Stop
-		if (! $cert) {
-		    throwError "DECRYPT_KEY_103" "Unable to open certificate Cert:\$location\My\$thumbprint"
-		}
-		if (! $cert.PrivateKey) {
-			Write-Host "[$scriptName] Are you running as administrator? Elevation is required to retrieve private key!"
-		    throwError "DECRYPT_KEY_106" "Unable to open private key for certificate Cert:\$location\My\$thumbprint"
-		}
-		# Write-Host "[$scriptName] `$object = $object"
-	    $key = $cert.PrivateKey.Decrypt($object.Key, $true)
-		if (! $cert) {
-		    throwError "DECRYPT_KEY_104" "Unable to decrypt using private key."
-		}
-		# Write-Host "[$scriptName] `$key = $key"
-	    $retrievedString = $object.Payload | ConvertTo-SecureString -Key $key
-		$plain = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($retrievedString))
-	} catch {
-		throw
-	} finally {
-	    if ($null -ne $key) { [array]::Clear($key, 0, $key.Length) }
 	}
 
 } else {
