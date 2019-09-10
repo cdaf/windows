79d2c4fe922a90130817019973efedfde2aa7e08
Jules <cdaf@outlook.co.nz>
2019-09-11 08:24:14 +1200

    Support for add and replace

--- automation/provisioning/addHOSTS.ps1
+++ automation/provisioning/addHOSTS.ps1
@@ -1,27 +1,64 @@
+Param (
+	[string]$ipAddress,
+	[string]$hostEntry
+)
+
+# Initialise
+cmd /c "exit 0"
 $scriptName = 'addHOSTS.ps1'
-Write-Host
-Write-Host "[$scriptName] ---------- start ----------"
-$ip = $args[0]
-if ($ip) {
-    Write-Host "[$scriptName] ip      : $ip"
-} else {
-    Write-Host "[$scriptName] ip no supplied"
-    exit 100
+
+# Common expression logging and error handling function, copied, not referenced to ensure atomic process
+function executeExpression ($expression) {
+	$error.clear()
+	Write-Host "[$scriptName] $expression"
+	try {
+		Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
+	} catch { echo $_.Exception|format-list -force; exit 2 }
+    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
+    if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
 }
 
-$address = $args[1]
-if ($address) {
-    Write-Host "[$scriptName] address : $address"
+# In memory replacement performed to avoid dead-lock scenario when trying to use Get-Content / Set-Content commandlets
+Write-Host "`n[$scriptName] ---------- start ----------`n"
+if ($ipAddress) {
+    Write-Host "[$scriptName] ipAddress : $ipAddress"
 } else {
-    Write-Host "[$scriptName] address not supplied"
-    exit 101
+    Write-Host "[$scriptName] ipAddress not supplied!" ; exit 777
 }
 
-$hosts = "$env:windir\System32\drivers\etc\hosts"
-Add-Content $hosts "`n$ip $address"
+if ($hostEntry) {
+    Write-Host "[$scriptName] hostEntry : $hostEntry"
+} else {
+    Write-Host "[$scriptName] hostEntry not supplied!" ; exit 776
+}
 
-Write-Host "[$scriptName] $hosts content"
-cat $hosts
+$hostsfile = 'C:\Windows\System32\drivers\etc\hosts'
+$transformed = @(Get-Content $hostsfile)
+$addEntry = $True
+$i = 0
+foreach ($record in $transformed) {
+    if ($record -match " ${hostEntry}") {
+        write-host "[$scriptName]   Replacing $($transformed[$i]) with $ipAddress ${hostEntry}"
+        $transformed[$i] = "$ipAddress ${hostEntry}"
+        $addEntry = $False
+        break
+    }
+    $i++
+}
 
-Write-Host
-Write-Host "[$scriptName] ---------- stop ----------"
+if ( $addEntry ) {
+	Write-Host "[$scriptName]   Existing entry not found, create $ipAddress ${hostEntry}"
+	$transformed += "$ipAddress ${hostEntry}"
+}
+
+Write-Host "[$scriptName] Replace $hostsfile"
+$stream = [System.IO.StreamWriter] $hostsfile
+foreach ($record in $transformed) {
+      $stream.WriteLine($record)
+}
+$stream.close()
+
+executeExpression "Get-Content $hostsfile" 
+
+Write-Host "`n[$scriptName] ---------- finish ----------`n"
