a0d4aa5ec8d75c9876a3f3c93287709bb6219f96
Jules <cdaf@outlook.co.nz>
2019-11-08 10:16:15 +1300

    Consistent application of timestamp in logs

--- bootstrap-arr.ps1
+++ bootstrap-arr.ps1
@@ -5,7 +5,7 @@ Param (
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	$error.clear()
-	Write-Host "$expression"
+	Write-Host "[$(date)] $expression"
 	try {
 		$output = Invoke-Expression $expression
 	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
