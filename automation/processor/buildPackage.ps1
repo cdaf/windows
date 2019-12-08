7c3fc239e61dbcce5ca4464eaf3c1d78fec6f536
Jules <cdaf@outlook.co.nz>
2019-12-09 09:59:59 +1300

    Prefix container build with &

--- automation/processor/buildPackage.ps1
+++ automation/processor/buildPackage.ps1
@@ -15,7 +15,7 @@ $scriptName = $MyInvocation.MyCommand.Name
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	$error.clear()
-	Write-Host "[$scriptName] $expression"
+	Write-Host "[$(date)] $expression"
 	try {
 		Invoke-Expression $expression
 	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
