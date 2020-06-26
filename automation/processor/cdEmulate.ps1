fd7aacd31174ddf3c357f2a65237d345f01db131
Jules <cdaf@outlook.co.nz>
2020-06-26 17:53:06 +1200

    Support for portable package

--- automation/processor/cdEmulate.ps1
+++ automation/processor/cdEmulate.ps1
@@ -93,8 +93,13 @@ if (Test-Path "$solutionRoot\delivery.bat") {
 	$cdProcess="$solutionRoot\delivery.bat"
 	write-host "$cdProcess (override)"
 } else {
-	$cdProcess="$AUTOMATIONROOT\processor\delivery.bat"
-	write-host "$cdProcess (default)"
+	$artifactPrefix=$(& $AUTOMATIONROOT\remote\getProperty.ps1 $solutionRoot\CDAF.solution 'artifactPrefix')
+	if ( $artifactPrefix ) {
+		$cdProcess = '.\release.ps1'
+	} else {
+		$cdProcess = "$AUTOMATIONROOT\processor\delivery.bat"
+		write-host "$cdProcess (default)"
+	}
 }
 # Packaging will ensure either the override or default delivery process is in the workspace root
 $cdInstruction="delivery.bat"
@@ -247,24 +252,24 @@ if ( $ACTION ) {
 	write-host '  Configure artifact dependency using "Build from the same chain"'
 	write-host '  Add a Configuration parameter from build.vcs.number to %env.BUILD_NUMBER%'
 	write-host '  Add Command Line build step'
-	write-host "    Custom script       : $workDirLocal\delivery.bat %env.TEAMCITY_BUILDCONF_NAME% %build.number%"
+	write-host "    Custom script       : $cdProcess %env.TEAMCITY_BUILDCONF_NAME% %build.number%"
 	write-host
 	write-host 'For Bamboo ...'
-	write-host "  Script file           : `${bamboo.build.working.directory}\$workDirLocal\$cdInstruction"
+	write-host "  Script file           : `${bamboo.build.working.directory}\$cdProcess"
 	write-host "  Argument              : `${bamboo.deploy.environment} `${bamboo.deploy.release}"
 	write-host
 	write-host 'For Jenkins (each environment requires a literal definition) ...'
-	write-host "  Command               : $workDirLocal\$cdInstruction <environment literal> %SVN_REVISION%"
+	write-host "  Command               : $cdProcess <environment literal> %SVN_REVISION%"
 	write-host
 	write-host 'For BuildMaster ... (Use "Deploy Artifact" to download to $WorkingDirectory, then use "PSExec" as follows)'
 	write-host '  Set workspace         : cd $WorkingDirectory'
-	write-host "  Run Delivery          : $workDirLocal\$cdInstruction `${EnvironmentName} `${ReleaseNumber}"
+	write-host "  Run Delivery          : $cdProcess `${EnvironmentName} `${ReleaseNumber}"
 	write-host
     write-host 'For Azure DevOps/Server (formerly Visual Studio Team Services (VSTS)/Team Foundation Server (TFS))'
 	write-host '  Verify the queue for each Environment definition, and ensure Environment names do not contain spaces.'
 	write-host '  Run an build with artefacts initially to load the workspace, which can then be navigated to for following configuration.'
 	write-host '  From an empty release configuration, bind to the existing build and within the stage, add a "PowerShell" step.'
-	write-host "    Command Filename    : `$(System.DefaultWorkingDirectory)/$solutionName/drop/$workDirLocal/delivery.ps1"
+	write-host "    Command Filename    : `$(System.DefaultWorkingDirectory)/$solutionName/drop/$cdProcess"
 	write-host '    Command arguments   : "$(Release.EnvironmentName)" "$(Release.ReleaseName)"'
 	write-host "    Working folder      : `$(System.DefaultWorkingDirectory)/$solutionName/drop"
 	write-host "    Release name format : $solutionName-`$(Build.BuildNumber)"
@@ -274,7 +279,7 @@ if ( $ACTION ) {
     write-host '  If using the sample .gitlab-ci.yml simply clone and change the Environment literal'
 	write-host '  variables:'
 	write-host '    ENV: "<environment>"'
-    write-host "    script: `"$workDirLocal/$cdInstruction `${ENV} `${CI_PIPELINE_ID}`""
+    write-host "    script: `"$cdProcess `${ENV} `${CI_PIPELINE_ID}`""
 	write-host '    environment: <environment>'
    	write-host
 	write-host "[$scriptName] -------------------------------------------------------"
