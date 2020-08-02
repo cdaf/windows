0768200b3401141f7a3c35a9046c751d76234201
Jules <cdaf@outlook.co.nz>
2020-08-02 13:25:58 +1200

    Initialise LASTEXITCODE

--- automation/provisioning/removeUser.ps1
+++ automation/provisioning/removeUser.ps1
@@ -1,14 +1,32 @@
+Param (
+	[string]$userName
+)
+
+cmd /c "exit 0"
+$Error.Clear()
+
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
-	$error.clear()
-	Write-Host "$expression"
+	Write-Host "[$(Get-Date)] $expression"
 	try {
-		$output = Invoke-Expression $expression
-	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; emailAndExit 1 }
-	} catch { echo $_.Exception|format-list -force; emailAndExit 2 }
-    if ( $error[0] ) { Write-Host "[$scriptName] `$error[0] = $error"; emailAndExit 3 }
-    if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
-    return $output
+		Invoke-Expression $expression
+	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; $error ; exit 1111 }
+	} catch { Write-Output $_.Exception|format-list -force; $error ; exit 1112 }
+    if ( $LASTEXITCODE ) {
+    	if ( $LASTEXITCODE -ne 0 ) {
+			Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE " -ForegroundColor Red ; $error ; exit $LASTEXITCODE
+		} else {
+			if ( $error ) {
+				Write-Host "[$scriptName][WARN] $Error array populated by `$LASTEXITCODE = $LASTEXITCODE, $error[] = $error`n" -ForegroundColor Yellow
+				$error.clear()
+			}
+		} 
+	} else {
+	    if ( $error ) {
+			Write-Host "[$scriptName][WARN] $Error array populated but LASTEXITCODE not set, $error[] = $error`n" -ForegroundColor Yellow
+			$error.clear()
+		}
+	}
 }
 
 function removeLocal ($userName) {
@@ -19,9 +37,8 @@ function removeLocal ($userName) {
 }
 
 $scriptName = 'removeUser.ps1'
-Write-Host "`n[$scriptName] Remove User from Domain or Workgroup, Windows Server 2012 and above`n"
+Write-Host "`n[$scriptName] Remove User from Domain or Workgroup, Windows Server 2012 and above. Pass .\ prefix for local user.`n"
 Write-Host "`n[$scriptName] ---------- start ----------"
-$userName = $args[0]
 if ($userName) {
     Write-Host "[$scriptName] userName : $userName"
 } else {
