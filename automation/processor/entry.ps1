0cc025807378fd4cc7b788d3b4412aeccf34bb7e
Jules Clements <cdaf@outlook.co.nz>
2020-09-03 14:49:03 +1200

    Unconditional passing of action to buildPackage (align with Linux)

--- automation/processor/entry.ps1
+++ automation/processor/entry.ps1
@@ -137,12 +137,7 @@ Write-Host "[$scriptName]   pwd            = $workspace"
 Write-Host "[$scriptName]   hostname       = $(hostname)" 
 Write-Host "[$scriptName]   whoami         = $(whoami)`n"
 
-if ( $branch -eq 'master' ) {
-	executeExpression "$automationRoot\processor\buildPackage.ps1 $BUILDNUMBER $branch $action -AUTOMATIONROOT $automationRoot"
-} else {
-	Write-Host "[$scriptName] Do not pass ACTION when executing feature branch (non-master)"
-	executeExpression "$automationRoot\processor\buildPackage.ps1 $BUILDNUMBER $branch -AUTOMATIONROOT $automationRoot"
-}
+executeExpression "$automationRoot\processor\buildPackage.ps1 $BUILDNUMBER $branch $action -AUTOMATIONROOT $automationRoot"
 
 if (( $branch -eq 'master' ) -or ( $branch -eq 'refs/heads/master' )) {
 	Write-Host "[$scriptName] Only perform container test in CI for branches, Master execution in CD pipeline"
