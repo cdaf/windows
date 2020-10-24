ca7e0cd022b53b88dda74ffe5d70e7e07f15f648
Jules <cdaf@outlook.co.nz>
2020-10-25 09:30:29 +1300

    Split development workstation config for 20h2 (no edge)

--- 20h2.ps1
+++ 20h2.ps1
@@ -38,12 +38,11 @@ executeExpression  "cd ~"
 executeExpression '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12'
 executeExpression  ". { iwr -useb http://cdaf.io/static/app/downloads/cdaf.ps1 } | iex"
 
-executeExpression  "~\automation\provisioning\base.ps1 'adoptopenjdk8 maven'"
-executeExpression  "~\automation\provisioning\base.ps1 eclipse -version 4.16" # Locked to the last release supporting Java 8
+executeExpression  "~\automation\provisioning\base.ps1 'adoptopenjdk11 maven eclipse'"
 executeExpression  "~\automation\provisioning\base.ps1 'nuget.commandline azure-cli visualstudio2019enterprise vscode'"
 
 executeExpression  "~\automation\provisioning\base.ps1 'nodejs.install git svn vnc-viewer putty winscp postman insomnia-rest-api-client'"
-executeExpression  "~\automation\provisioning\base.ps1 'citrix-receiver zoom microsoft-edge'"
+executeExpression  "~\automation\provisioning\base.ps1 'citrix-receiver zoom'"
 executeExpression  "~\automation\provisioning\base.ps1 'googlechrome' -checksum ignore" # Google does not provide a static download, so checksum can briefly fail on new releases
 
 executeExpression  "~\automation\provisioning\base.ps1 'vagrant' -autoReboot yes"
