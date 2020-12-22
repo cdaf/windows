1a8ee2c3f7d63efcdbf107a52be2c0f028bfff08
Jules <cdaf@outlook.co.nz>
2020-12-23 10:05:14 +1300

    Apply automatic masking of key, secret, pat and password variable names

--- automation/provisioning/setenv.ps1
+++ automation/provisioning/setenv.ps1
@@ -8,6 +8,10 @@ Param (
 cmd /c "exit 0"
 $scriptName = 'setenv.ps1'
 
+function mask ($value) {
+	return (Get-FileHash -InputStream $([IO.MemoryStream]::new([byte[]][char[]]$value)) -Algorithm MD5).Hash
+}
+
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	$error.clear()
@@ -15,37 +19,36 @@ function executeExpression ($expression) {
 	try {
 		Invoke-Expression $expression
 	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
-	} catch { echo $_.Exception|format-list -force; exit 2 }
+	} catch { Write-Output $_.Exception|format-list -force; exit 2 }
     if ( $error ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
     if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
 }
 
 Write-Host "`n[$scriptName] ---------- start ----------"
-if ($variable) {
-    Write-Host "[$scriptName] variable : $variable"
+if ( $variable ) {
+    Write-Host "[$scriptName]   variable : $variable"
 } else {
 	$mediaDir = "$env:TEMP"
-    Write-Host "[$scriptName] mediaDir : $mediaDir (default)"
+    Write-Host "[$scriptName]   mediaDir : $mediaDir (default)"
 }
 
-if ($value) {
-	if ($secret -eq 'yes') {
-		Write-Host "[$scriptName] value    : **********************"
+if ( $value ) {
+	if (( $secret ) -or ( $variable -like '*key*' ) -or ( $variable -like '*secret*' ) -or ( $variable -like '*password*' ) -or ( $variable -like '*pat*' )) {
+		Write-Host "[$scriptName]   value    : $(mask $value) (MD5 mask)"
 	} else {
-		Write-Host "[$scriptName] value    : $value"
+		Write-Host "[$scriptName]   value    : $value"
 	}
 } else {
     Write-Host "[$scriptName] value required, exiting!"
     exit 101
 }
 
-if ($target) {
-    Write-Host "[$scriptName] target   : $target"
+if ( $target ) {
+    Write-Host "[$scriptName]   target   : $target (choices user or machine)"
 } else {
 	$target = 'user'
-    Write-Host "[$scriptName] target   : $target (default, choices user or machine)"
+    Write-Host "[$scriptName]   target   : $target (default, choices user or machine)"
 }
-
 
 Write-Host
 executeExpression "[Environment]::SetEnvironmentVariable(`'$variable`', `$value, `'$target`')"
