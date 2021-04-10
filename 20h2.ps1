da042f53bce3c715301a8766a537682c98dfce6d
Jules <cdaf@outlook.co.nz>
2021-04-11 08:40:05 +1200

    Apply virtualisation extension

--- 20h2.ps1
+++ 20h2.ps1
@@ -1,49 +1,143 @@
+Param (
+	[string]$virtualisation,
+	[string]$vagrantPass
+)
+
 cmd /c "exit 0"
 $Error.Clear()
+
+# Consolidated Error processing function
+function ERRMSG ($message, $exitcode) {
+	if ( $exitcode ) {
+		Write-Host "`n[$scriptName]$message" -ForegroundColor Red
+	} else {
+		Write-Host "`n[$scriptName]$message" -ForegroundColor Yellow
+	}
+	if ( $error ) {
+		$i = 0
+		foreach ( $item in $Error )
+		{
+			Write-Host "`$Error[$i] $item"
+			$i++
+		}
+		$Error.clear()
+	}
+	if ( $env:CDAF_ERROR_DIAG ) {
+		Write-Host "`n[$scriptName] Invoke custom diag `$env:CDAF_ERROR_DIAG = $env:CDAF_ERROR_DIAG`n"
+		Invoke-Expression $env:CDAF_ERROR_DIAG
+	}
+	if ( $exitcode ) {
+		Write-Host "`n[$scriptName] Exit with LASTEXITCODE = $exitcode`n" -ForegroundColor Red
+		exit $exitcode
+	}
+}
 
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	Write-Host "[$(Get-Date)] $expression"
 	try {
 		Invoke-Expression $expression
-	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; $error ; exit 1111 }
-	} catch { Write-Output $_.Exception|format-list -force; $error ; exit 1112 }
+	    if(!$?) { ERRMSG "[TRAP] `$? = $?" 1211 }
+	} catch {
+		$message = $_.Exception.Message
+		$_.Exception | format-list -force
+		$_.Exception.StackTrace
+		if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) {
+			ERRMSG "[EXCEPTION] $message" $LASTEXITCODE
+		} else {
+			ERRMSG "[EXCEPTION] $message" 1212
+		}
+	}
     if ( $LASTEXITCODE ) {
     	if ( $LASTEXITCODE -ne 0 ) {
-			Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE " -ForegroundColor Red ; $error ; exit $LASTEXITCODE
+			ERRMSG "[EXIT] `$LASTEXITCODE is $LASTEXITCODE" $LASTEXITCODE
 		} else {
 			if ( $error ) {
-				Write-Host "[$scriptName][WARN] $Error array populated by `$LASTEXITCODE = $LASTEXITCODE, $error[] = $error`n" -ForegroundColor Yellow
-				$error.clear()
+				ERRMSG "[WARN] `$LASTEXITCODE is $LASTEXITCODE, but standard error populated"
 			}
 		} 
 	} else {
 	    if ( $error ) {
-			Write-Host "[$scriptName][WARN] $Error array populated but LASTEXITCODE not set, $error[] = $error`n" -ForegroundColor Yellow
-			$error.clear()
+	    	if ( $env:CDAF_IGNORE_WARNING -eq 'no' ) {
+				ERRMSG "[ERROR] `$env:CDAF_IGNORE_WARNING is 'no' so exiting" 1213
+	    	} else {
+				ERRMSG "[WARN] `$LASTEXITCODE not set, but standard error populated"
+	    	}
 		}
 	}
+}
+
+function MD5MSK ($value) {
+	(Get-FileHash -InputStream $([IO.MemoryStream]::new([byte[]][char[]]$value)) -Algorithm MD5).Hash
 }
 
 $scriptName = 'dev.ps1'
 
 Write-Host "`n[$scriptName] ---------- start ----------"
+if ($virtualisation) {
+    writeLog "virtualisation : $virtualisation"
+} else {
+    writeLog "virtualisation : (not specified, developer configuration)"
+}
+if ($vagrantPass) {
+    writeLog "vagrantPass    : $(MD5MSK $vagrantPass) (MD5 mask)"
+} else {
+    writeLog "vagrantPass    : (not specified)"
+}
+
 if ( $env:http_proxy ) {
     executeExpression "[system.net.webrequest]::defaultwebproxy = New-Object system.net.webproxy('$env:http_proxy')"
 } else {
     executeExpression '(New-Object System.Net.WebClient).Proxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials' 
 }
 
-executeExpression  "cd ~"
-executeExpression '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12'
-executeExpression  ". { iwr -useb http://cdaf.io/static/app/downloads/cdaf.ps1 } | iex"
+if ( $virtualisation -eq 'hyperv' ) {
 
-executeExpression  "~\automation\provisioning\base.ps1 'adoptopenjdk11 maven eclipse'"
-executeExpression  "~\automation\provisioning\base.ps1 'nuget.commandline azure-cli visualstudio2019enterprise vscode'"
+    executeExpression "setenv.ps1 VAGRANT_DEFAULT_PROVIDER hyperv"
+    executeExpression "setenv.ps1 VAGRANT_SMB_USER $env:USERNAME"
+	if ($vagrantPass) {
+		executeExpression "setenv.ps1 VAGRANT_SMB_PASS $vagrantPass"
+	}
+ 
+    executeExpression "Dism /online /enable-feature /all /featurename:Microsoft-Hyper-V /NoRestart"
+    executeExpression "Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart"
+    executeExpression "base.ps1 'docker-desktop wsl2'"
+    executeExpression "reboot /r /t 0"
 
-executeExpression  "~\automation\provisioning\base.ps1 'nodejs.install git svn vnc-viewer putty winscp postman insomnia-rest-api-client'"
-executeExpression  "~\automation\provisioning\base.ps1 'googlechrome' -checksum ignore" # Google does not provide a static download, so checksum can briefly fail on new releases
+} elseif ( $virtualisation -eq 'virtualbox' ) {
 
-executeExpression  "~\automation\provisioning\base.ps1 'vagrant' -autoReboot yes"
+    executeExpression "addHOSTS.ps1 172.16.17.90  cbe.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.98  dc.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.99  db.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.100 agent.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.100 build.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.101 server-1.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.102 server-2.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.101 windows-1.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.102 windows-2.mshome.net"
+    executeExpression "addHOSTS.ps1 172.16.17.103 app.mshome.net"
+ 
+    executeExpression "base.ps1 'virtualbox'"
+    executeExpression "reboot /r /t 0"
+	
+} else {
+
+	executeExpression  "cd ~"
+	executeExpression  "mkdir git"
+	executeExpression  "cd .\git\"
+	executeExpression  "git clone https://github.com/cdaf/windows.git"
+	executeExpression  "& ${env:USERPROFILE}\git\windows\automation\provisioning\addPath.ps1 ${env:USERPROFILE}\git\windows\automation\provisioning User"
+	executeExpression  "& ${env:USERPROFILE}\git\windows\automation\provisioning\addPath.ps1 ${env:USERPROFILE}\git\windows\automation User"
+
+	executeExpression  "cd ~"
+
+	executeExpression  "base.ps1 'adoptopenjdk11 maven eclipse'"
+	executeExpression  "base.ps1 'nuget.commandline azure-cli visualstudio2019enterprise vscode'"
+
+	executeExpression  "base.ps1 'nodejs.install git svn vnc-viewer putty winscp postman insomnia-rest-api-client'"
+	executeExpression  "base.ps1 'googlechrome' -checksum ignore" # Google does not provide a static download, so checksum can briefly fail on new releases
+
+	executeExpression  "base.ps1 'vagrant' -autoReboot yes"
+}
 
 Write-Host "`n[$scriptName] ---------- stop ----------"
