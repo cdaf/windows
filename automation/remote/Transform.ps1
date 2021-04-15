fdeffc140389876e8ed1031c876efb05c01ca709
Jules <cdaf@outlook.co.nz>
2021-04-15 18:13:34 +1200

    Support value containing comma

--- automation/remote/Transform.ps1
+++ automation/remote/Transform.ps1
@@ -96,7 +96,11 @@ Foreach ($line in $propertiesArray) {
                     $i++
                 }
             } else { # If token file is not supplied, echo strings for instantiating as variables (cannot instantiate here as they will be out of scope)
-                $loadVariable = "`$$name = '$value'"
+            	if ( $value.LastIndexOf("-") -gt 0 ) {
+	                $loadVariable = "`$$name = `"$value`""
+            	} else {
+	                $loadVariable = "`$$name = '$value'"
+	            }
                 Write-Output "$loadVariable"
                 write-host "[$scriptName]   $name = $value"
             }
