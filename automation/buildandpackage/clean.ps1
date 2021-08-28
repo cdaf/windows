2418a2ab584f936b493f68e35d81e3d79d82dbc2
Jules <cdaf@outlook.co.nz>
2021-08-28 12:38:32 +1200

    Substring to cater for branch names containing underscores

--- automation/buildandpackage/clean.ps1
+++ automation/buildandpackage/clean.ps1
@@ -10,69 +10,105 @@ $scriptName = 'clean.ps1'
 # Common expression logging and error handling function, copied, not referenced to ensure atomic process
 function executeExpression ($expression) {
 	Write-Host "[$(Get-Date)] $expression"
-	try {
+	try
+	{
 		Invoke-Expression "$expression 2> `$null"
-	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; $error ; exit 1211 }
-	} catch {
+		if (!$?) { Write-Host "[$scriptName] `$? = $?"; $error ; exit 1211 }
+	}
+	catch
+	{
 		Write-Host "[$scriptName][EXCEPTION] List exception and error array (if populated) and exit with LASTEXITCODE 1212" -ForegroundColor Red
-		Write-Host $_.Exception|format-list -force
+		Write-Host $_.Exception | format-list -force
 		if ( $error ) { Write-Host "[$scriptName][ERROR] `$Error = $Error" ; $Error.clear() }
 		exit 1212
 	}
-    if ( $LASTEXITCODE ) {
-    	if ( $LASTEXITCODE -ne 0 ) {
+	if ( $LASTEXITCODE )
+	{
+		if ( $LASTEXITCODE -ne 0 )
+		{
 			Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE " -ForegroundColor Red
 			if ( $error ) { Write-Host "[$scriptName][ERROR] `$Error = $Error" ; $Error.clear() }
 			exit $LASTEXITCODE
-		} else {
-			if ( $error ) {
+		}
+		else
+		{
+			if ( $error )
+			{
 				Write-Host "[$scriptName][WARN] $Error array populated by `$LASTEXITCODE = $LASTEXITCODE error follows...`n" -ForegroundColor Yellow
 				Write-Host "[$scriptName][WARN] `$Error = $Error" ; $Error.clear()
 			}
 		} 
-	} else {
-	    if ( $error ) {
-	    	if ( $env:CDAF_IGNORE_WARNING -eq 'no' ) {
+	}
+ else {
+		if ( $error )
+		{
+			if ( $env:CDAF_IGNORE_WARNING -eq 'no' )
+			{
 				Write-Host "[$scriptName][ERROR] `$Error = $error"; $Error.clear()
 				Write-Host "[$scriptName][ERROR] `$env:CDAF_IGNORE_WARNING is 'no' so exiting with LASTEXITCODE 1213 ..."; exit 1213
-	    	} else {
-		    	Write-Host "[$scriptName][WARN] `$Error = $error" ; $Error.clear()
-	    	}
+			}
+			else
+			{
+				Write-Host "[$scriptName][WARN] `$Error = $error" ; $Error.clear()
+			}
 		}
 	}
 }
 
 Write-Host "`n[$scriptName] --- start ---"
 if ($SOLUTION) {
-    Write-Host "[$scriptName]   SOLUTION      : $SOLUTION"
-} else {
-    Write-Host "[$scriptName] SOLUTION not passed!"; exit 1394
+	Write-Host "[$scriptName]   SOLUTION      : $SOLUTION"
+}
+else {
+	Write-Host "[$scriptName] SOLUTION not passed!"; exit 1394
 }
 
-if ($passedArray) {
+if ($passedArray)
+{
 	$aphaNumArray = @()
-	foreach ($remoteBranch in $passedArray) { # verify array contents
+	foreach ($remoteBranch in $passedArray)
+	{
+		# verify array contents
 		$i++
 		Write-Host "[$scriptName]   remoteBranch${i} : $remoteBranch"
-		if ( $remoteBranch -match '/' ) {
+		if ( $remoteBranch -match '/' )
+		{
 			$remoteBranch = $remoteBranch.Split('/')[-1]
 		}
 		$aphaNumArray += ($remoteBranch -replace '[^a-zA-Z0-9]', '').ToLower()
 	}
-} else {
-    Write-Host "[$scriptName] passedArray not passed!"; exit 1395
+}
+else
+{
+	Write-Host "[$scriptName] passedArray not passed!"; exit 1395
 }
 
 Write-Host "`n[$scriptName] docker images `"${SOLUTION}*`" --format `"{{.Repository}}:{{.ID}}`"`n"
-foreach ( $image in $(docker images "${SOLUTION}*" --format "{{.Repository}}:{{.ID}}" 2>$null )) {
-	$imageBranch,$id = $image.Split(':')
+foreach ( $image in $(docker images "${SOLUTION}*" --format "{{.Repository}}:{{.ID}}" 2>$null ))
+{
+	$imageBranch, $id = $image.Split(':')
 	if ( $imageBranch ) {
-		$imageCompare = ($imageBranch.Substring($imageBranch.IndexOf('_')+1 , $imageBranch.Length - ($imageBranch.Length - $imageBranch.LastIndexOf('_')) - $imageBranch.IndexOf('_') -1)).ToLower()
-		if ( $aphaNumArray.Contains($imageCompare) ) {
-			Write-Host "  keep $imageBranch"
-		} else {
-			Write-Host "  docker rmi -f ${imageBranch}"
-			docker rmi ${id}
+		# Substring to cater for branch names containing underscores
+		$offset = $imageBranch.IndexOf('_') + 1
+		# Trim the solution and image name, leaving only the branch name
+		$length = $imageBranch.Length - $offset - ($imageBranch.Length - $imageBranch.LastIndexOf('_'))
+		if ( $offset -lt 0 ) {
+			Write-Host "`n[$scriptName] $imageBranch offset is $offset, skipping...`"`n"
+		}
+		elseif ( $length -lt 0 ) {
+			Write-Host "`n[$scriptName] $imageBranch length is $length, skipping...`"`n"
+		}
+		else {
+			# Write-Host "[DEBUG] `$imageBranch, `$id, `$offset, `$length = $imageBranch, $id, $offset, $length"
+			$imageCompare = ($imageBranch.Substring($offset, $length).ToLower())
+			# Write-Host "[DEBUG] `$imageCompare = $imageCompare"
+			if ( $aphaNumArray.Contains($imageCompare) ) {
+				Write-Host "  keep $imageBranch"
+			}
+			else {
+				Write-Host "  docker rmi -f ${imageBranch}"
+				docker rmi ${id}
+			}
 		}
 	}
 }
