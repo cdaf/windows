e613a27a80691cf5aff9e7b10a0cfa16311563ef
Continuous Delivery Automation Framework <cdaf@outlook.co.nz>
2022-04-11 20:03:39 +1200

    Update 20h2.ps1
    Trim some bloat

--- 20h2.ps1
+++ 20h2.ps1
@@ -136,6 +136,7 @@ if ( $virtualisation -eq 'hyperv' ) {
 	
 } else {
 
+	executeExpression  "Get-AppxPackage Microsoft.YourPhone -AllUsers | Remove-AppxPackage"
 	executeExpression  "(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null"
 	executeExpression  "(Get-WmiObject -Class 'Win32_TSGeneralSetting' -Namespace root\cimv2\TerminalServices -Filter `"TerminalName='RDP-tcp'`").SetUserAuthenticationRequired(0) | Out-Null"
 	executeExpression  "Get-NetFirewallRule -DisplayName `"Remote Desktop*`" | Set-NetFirewallRule -enabled true"
