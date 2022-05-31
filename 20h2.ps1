185677f833cc2a0683fe610b53bac4f1b1460c64
Continuous Delivery Automation Framework <cdaf@outlook.co.nz>
2022-05-31 17:49:29 +1200

    Guidance for SMB password supply

--- 20h2.ps1
+++ 20h2.ps1
@@ -1,6 +1,6 @@
 #  . { iwr -useb https://raw.githubusercontent.com/cdaf/windows/master/20h2.ps1 } | iex
 # iwr -useb https://raw.githubusercontent.com/cdaf/windows/master/20h2.ps1 -o 20h2.ps1
-# .\20h2.ps1 hyperv
+# .\20h2.ps1 hyperv <smbpassword>
 # .\20h2.ps1 virtualbox
 
 Param (
