666b52cbe3cb70c0179f163a0d4a9855fb997db3
Continuous Delivery Automation Framework <cdaf@outlook.co.nz>
2023-01-24 09:02:08 +1300

    Do not clone CDAF itself

--- 22h2.ps1
+++ 22h2.ps1
@@ -177,17 +177,6 @@ if ( $virtualisation -eq 'hyperv' ) {
     executeExpression "git config --global core.autocrlf false"
     executeExpression ".\automation\provisioning\base.ps1 'googlechrome' -checksum ignore" # Google does not provide a static download, so checksum can briefly fail on new releases
 
-    executeExpression "Remove-Item -Recurse -Force automation"
-    if ( Test-Path git ) {
-        Write-Host "Git directory exists"
-    } else {
-        executeExpression  "mkdir git"
-    }
-    executeExpression "cd .\git\"
-    executeExpression "git clone https://github.com/cdaf/windows.git"
-    executeExpression "& ${env:USERPROFILE}\git\windows\automation\provisioning\addPath.ps1 ${env:USERPROFILE}\git\windows\automation\provisioning User"
-    executeExpression "& ${env:USERPROFILE}\git\windows\automation\provisioning\addPath.ps1 ${env:USERPROFILE}\git\windows\automation User"
-
     $extensions = @()
     $extensions += "42crunch.vscode-openapi"
     $extensions += "bierner.markdown-mermaid"
