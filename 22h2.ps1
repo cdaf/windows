dde120fa999c987a9c49faf297ec25057a4072f8
Continuous Delivery Automation Framework <cdaf@outlook.co.nz>
2023-01-24 09:44:20 +1300

    Do not fail on disabling beep

--- 22h2.ps1
+++ 22h2.ps1
@@ -156,7 +156,8 @@ if ( $virtualisation -eq 'hyperv' ) {
 	
 } else {
 
-    executeExpression "Set-Service beep -StartupType disabled"
+    Write-Host "Set-Service beep -StartupType disabled"
+    Set-Service beep -StartupType disabled
     executeExpression "Get-AppxPackage Microsoft.YourPhone -AllUsers | Remove-AppxPackage"
     executeExpression "(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null"
     executeExpression "(Get-WmiObject -Class 'Win32_TSGeneralSetting' -Namespace root\cimv2\TerminalServices -Filter `"TerminalName='RDP-tcp'`").SetUserAuthenticationRequired(0) | Out-Null"
