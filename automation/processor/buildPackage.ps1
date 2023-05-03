32c654be9933fd3f35bd5c31c4ddb0ecca15830c
Jules Clements <jules.clements@southerncross.co.nz>
2023-05-04 10:48:11 +1200

    Perform file stream for base64 package restore

--- automation/processor/buildPackage.ps1
+++ automation/processor/buildPackage.ps1
@@ -700,10 +700,7 @@ if ( $ACTION -ne 'container_build' ) {
 			Add-Content "release.ps1" 'if ( Test-Path "TasksLocal" ) { Remove-Item -Recurse TasksLocal }'
 			Add-Content "release.ps1" "Remove-Item ${SOLUTION}*.zip" # remote package
 			Add-Content "release.ps1" 'Write-Host "[$(Get-Date)] Extracting embedded package file ..."'
-			Add-Content "release.ps1" '$Content = [System.Convert]::FromBase64String($Base64)'
-			Add-Content "release.ps1" "Set-Content -Path '${compressedArtefact}' -Value `$Content -Encoding Byte"
-			Add-Content "release.ps1" "`$packagesize = (get-item '${compressedArtefact}').Length/1MB"
-			Add-Content "release.ps1" "if ( `$packagesize -gt 50 ) { Write-Host `"[`$(Get-Date)] pause to release memory due to large package size (`$packagesize MB) ...`" ; sleep 20 }"
+			Add-Content "release.ps1" "[IO.File]::WriteAllBytes(`"`$pwd\${compressedArtefact}`",[System.Convert]::FromBase64String(`$Base64))"
 
 			Add-Content "release.ps1" 'Write-Host "[$(Get-Date)] Decompressing package file ..."'
 			if ( $packageMethod -eq 'tarball' ) {
@@ -713,7 +710,6 @@ if ( $ACTION -ne 'container_build' ) {
 				Add-Content "release.ps1" 'Add-Type -AssemblyName System.IO.Compression.FileSystem'
 				Add-Content "release.ps1" "[System.IO.Compression.ZipFile]::ExtractToDirectory(`"`$PWD\${compressedArtefact}`", `"`$PWD`")"
 			}
-			Add-Content "release.ps1" "if ( `$packagesize -gt 50 ) { Write-Host `"[`$(Get-Date)] pause to release memory ...`" ; sleep 20 }"
 
 			Add-Content "release.ps1" 'Write-Host "[$(Get-Date)] Execute Deployment ..."'
 			Add-Content "release.ps1" '.\TasksLocal\delivery.bat "$ENVIRONMENT" "$RELEASE" "$OPT_ARG"'
