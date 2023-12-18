5e82fe5638915a6229cd84d4b0c01e6ee3e40ade
Continuous Delivery Automation Framework <cdaf@outlook.co.nz>
2023-12-18 13:02:05 +1300

    Apply WSL kernel download to deeplink

--- win-dev.ps1
+++ win-dev.ps1
@@ -183,7 +183,7 @@ if (( $virtualisation -eq 'hyperv' ) -or ( $virtualisation -eq 'docker' )) {
     if ( $virtualisation -eq 'docker' ) {
         executeExpression ".\automation\provisioning\base.ps1 docker-desktop"
         executeExpression ".\automation\provisioning\base.ps1 wsl2"
-        executeExpression "curl.exe -L https://aka.ms/wsl2kernel -o wsl_update_x64.msi"
+        executeExpression "curl.exe -L https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -o wsl_update_x64.msi"
         executeExpression ".\automation\provisioning\installMSI.ps1 wsl_update_x64.msi"
         executeExpression "wsl --set-default-version 2"
         executeExpression "curl.exe -L https://aka.ms/wslubuntu2004 -o wslubuntu2004.appx"
