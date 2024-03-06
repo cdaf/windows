999c71696e738b3f6a82984ee6b6b496ba779dfc
Jules <cdaf@outlook.co.nz>
2024-03-06 14:31:32 +1300

    Keep test container running even if test fails

--- samples/targetlessCD/compose/test/executeTest.ps1
+++ samples/targetlessCD/compose/test/executeTest.ps1
@@ -6,50 +6,90 @@ Param (
 cmd /c "exit 0"
 $scriptName = 'executeTest.ps1'
 
-# Common expression logging and error handling function, copied, not referenced to ensure atomic process
-function executeExpression ($expression) {
-	$error.clear()
-	Write-Host "[$scriptName] $expression"
-	try {
-		Invoke-Expression $expression
-	    if(!$?) { Write-Host "[$scriptName] `$? = $?"; exit 1 }
-	} catch { Write-Output $_.Exception|format-list -force; exit 2 }
-    if ( $error ) { Write-Host "[$scriptName] `$error[0] = $error"; exit 3 }
-    if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) { Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE "; exit $LASTEXITCODE }
+# Customised Error Hander to keep container alive, even if test fails.
+function ERRMSG ($message, $exitcode) {
+	if ( $exitcode ) {
+		if ( $exitcode ) {
+			Write-Host "`n[$scriptName]$message" -ForegroundColor Red
+		} else {
+			Write-Host "`n[$scriptName] ERRMSG triggered without message parameter." -ForegroundColor Red
+		}
+	} else {
+		if ( $exitcode ) {
+			Write-Warning "`n[$scriptName]$message"
+		} else {
+			Write-Warning "`n[$scriptName] ERRMSG triggered without message parameter."
+		}
+	}
+	if ( $error ) {
+		$i = 0
+		foreach ( $item in $Error )
+		{
+			Write-Host "`$Error[$i] $item"
+			$i++
+		}
+		$Error.clear()
+	}
+	if ( $exitcode ) {
+		if ( $env:CDAF_ERROR_DIAG ) {
+			Write-Host "`n[$scriptName] Invoke custom diag `$env:CDAF_ERROR_DIAG = $env:CDAF_ERROR_DIAG`n"
+			try {
+				Invoke-Expression $env:CDAF_ERROR_DIAG
+			    if(!$?) { Write-Host "[CDAF_ERROR_DIAG] `$? = $?" }
+			} catch {
+				$message = $_.Exception.Message
+				$_.Exception | format-list -force
+			}
+		    if ( $LASTEXITCODE ) {
+		    	if ( $LASTEXITCODE -ne 0 ) {
+					Write-Host "[CDAF_ERROR_DIAG][EXIT] `$LASTEXITCODE is $LASTEXITCODE"
+				}
+			}
+		}
+		Write-Host "`n[$scriptName] Exit with LASTEXITCODE = $exitcode`n" -ForegroundColor Red
+#		exit $exitcode
+		$script:test_failure = 'yes'
+	}
 }
 
-function executeRetry ($expression) {
-	$wait = 10
-	$retryMax = 3
-	$retryCount = 0
-	$exitCode = 1 # Any value other than 0 to enter the loop
-	while (( $retryCount -le $retryMax ) -and ($exitCode -ne 0)) {
-		$exitCode = 0
-		$error.clear()
-		Write-Host "[$scriptName][$retryCount] $expression"
-		try {
-			Invoke-Expression $expression
-		    if(!$?) { Write-Host "[$scriptName] `$? = $?"; $exitCode = 1 }
-			if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) {
-				$exitCode = $LASTEXITCODE; Write-Host "[$scriptName] `$LASTEXITCODE = $LASTEXITCODE " -ForegroundColor Yellow; cmd /c "exit 0"
-			}
-		} catch { 
+# Customised Expression Executor to log success message if no errors handled.
+function executeExpression ($expression) {
+	Write-Host "[$(Get-Date)] $expression"
+	try {
+		Invoke-Expression $expression
+	    if(!$?) { ERRMSG "[TRAP] `$? = $?" 1211 }
+	} catch {
+		$message = $_.Exception.Message
+		$_.Exception | format-list -force
+		$_.Exception.StackTrace
+		if (( $LASTEXITCODE ) -and ( $LASTEXITCODE -ne 0 )) {
+			ERRMSG "[EXEC][EXCEPTION] $message" $LASTEXITCODE
+		} else {
+			ERRMSG "[EXEC][EXCEPTION] $message" 1212
+		}
+	}
+    if ( $LASTEXITCODE ) {
+    	if ( $LASTEXITCODE -ne 0 ) {
+			ERRMSG "[EXEC][EXIT] `$LASTEXITCODE is $LASTEXITCODE" $LASTEXITCODE
+		} else {
 			if ( $error ) {
-				Write-Host "[$scriptName] `$error[0] = $error" ; $error.clear() ; $exitCode = 3
-			} else {
-				Write-Output $_.Exception|format-list -force ; $exitCode = 2
+				ERRMSG "[EXEC][WARN] `$LASTEXITCODE is $LASTEXITCODE, but standard error populated"
 			}
+		} 
+	} else {
+	    if ( $error ) {
+	    	if ( $env:CDAF_IGNORE_WARNING -eq 'no' ) {
+				ERRMSG "[EXEC][ERROR] `$env:CDAF_IGNORE_WARNING is 'no' so exiting" 1213
+	    	} else {
+				ERRMSG "[EXEC][WARN] `$LASTEXITCODE not set, but standard error populated"
+	    	}
 		}
-	    if ($exitCode -gt 0) {
-			if ($retryCount -ge $retryMax ) {
-				Write-Host "[$scriptName] Retry maximum ($retryCount) reached, exiting with code $exitCode"; exit $exitCode
-			} else {
-				$retryCount += 1
-				Write-Host "[$scriptName] Wait $wait seconds, then retry $retryCount of $retryMax"
-				Start-Sleep $wait
-			}
-		}
-    }
+	}
+	if ( $script:test_failure ) {
+		Write-Host "`n[$scriptName] CDAF_DELIVERY_FAILURE"
+	} else {
+		Write-Host "`n[$scriptName] Automated Test Execution completed successfully."
+	}
 }
 
 Write-Host "`n[$scriptName] ---------- start ----------`n"
@@ -62,12 +102,13 @@ if ($ENVIRONMENT) {
 Write-Host "`n[$scriptName] Execute CDAF Delivery`n"
 executeExpression ".\TasksLocal\delivery.bat $ENVIRONMENT"
 
-Write-Host "`n[$scriptName] Automated Test Execution completed successfully."
+Write-Host "`n[$scriptName] ---------- Watch System Events to keep container alive ----------"
 
-Write-Host '---------- Watch Windows Events to keep container alive ----------'
+$idx = (get-eventlog -LogName System -Newest 1).Index
+
 while ($true) {
-  start-sleep -Seconds 10
-  $idx2  = (Get-EventLog -LogName System -newest 1).index
-  get-eventlog -logname system -newest ($idx2 - $idx) |  sort index
-  $idx = $idx2
+	start-sleep -Seconds 1
+	$idx2  = (Get-EventLog -LogName System -newest 1).index
+	get-eventlog -logname system -newest ($idx2 - $idx) |  Sort-Object index
+	$idx = $idx2
 }
